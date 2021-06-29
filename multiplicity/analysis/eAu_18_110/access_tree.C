9b1332bdc0d021c0803b6d8c577af55190c2746f
unknown <devereauxk@berkeley.edu>
2021-06-29 00:41:18 -0700

    mul changes

--- multiplicity/analysis/eAu_18_110/access_tree.C
+++ multiplicity/analysis/eAu_18_110/access_tree.C
@@ -22,28 +22,32 @@ void access_tree(const char* inFile, const char* outFile)
   //Access event Branch
   tree->SetBranchAddress("event",&event); //Note &event, even with event being a pointer
 
-  TH1D* h1d_kaon = new TH1D("h1d_kaon","charged kaon multiplicity",100,0,12);
-  h1d_kaon->Sumw2(); // to handle error propagation correctly later
+  TH2D* h2d_kaon = new TH2D("h2d_kaon","charged kaon multiplicity",100,0,12,0,12);
+  h2d_kaon->Sumw2(); // to handle error propagation correctly later
 
-  TH1D* h1d_pion = new TH1D("h1d_pion","charged pion multiplicity",100,0,10000);
-  h1d_pion->Sumw2(); // to handle error propagation correctly later
+  TH2D* h2d_pion = new T21D("h2d_pion","charged pion multiplicity",100,0,10000,0,10000);
+  h2d_pion->Sumw2(); // to handle error propagation correctly later
 
-  TH1D* h1d_proton = new TH1D("h1d_proton","proton multiplicity",100,0,400);
+  TH1D* h1d_proton = new TH1D("h1d_proton","proton multiplicity",100,0,200);
   h1d_proton->Sumw2(); // to handle error propagation correctly later
 
   //Define Some Variables
   Int_t nParticles(0);
-  Int_t status;
   Int_t id;
-  Int_t nChargedKaons;
-  Int_t nChargedPions;
+  Int_t status;
+  Int_t nPosKaons;
+  Int_t nNegKaons;
+  Int_t nPosPions;
+  Int_t nNegPions;
   Int_t nProtons;
 
   //Loop Over Events
   for(Int_t i = 0; i < nEntries; i++) {
 
-    nChargedKaons = 0;
-    nChargedKaons = 0;
+    nPosKaons = 0;
+    nNegKaons = 0;
+    nPosPions = 0;
+    nNegPions = 0;
     nProtons = 0;
 
     tree->GetEntry(i);
@@ -58,17 +62,29 @@ void access_tree(const char* inFile, const char* outFile)
       status = (Int_t) particle->GetStatus(); //Can also do particle->KS
       id = (Int_t) particle->Id();
 
-      if (id == 321 || id == -321) { // if charged kaon (+ or -, resp.)
-        nChargedKaons++;
-      } else if (id == 211 || id == -211) { // if charged pion (+ or -, resp.)
-        nChargedPions++;
-      } else if (id == 2212) { // if proton
-        nProtons++;
+      switch(id) {
+        case 321:
+          nPosKaons++;
+          break;
+        case -321:
+          nNegKaons++:
+          break;
+        case 211:
+          nPosPions++;
+          break;
+        case -211:
+          nNegPions++;
+          break;
+        case 2212:
+          nProtons++;
+          break;
+        default: break;
       }
+
     }
 
-    h1d_kaon->Fill(nChargedKaons);
-    h1d_pion->Fill(nChargedPions);
+    h2d_kaon->Fill(nPosKaons, nNegKaons);
+    h2d_pion->Fill(nPosPions, nNegPions);
     h1d_proton->Fill(nProtons);
 
   }
@@ -76,8 +92,8 @@ void access_tree(const char* inFile, const char* outFile)
   cout<<"-------------------------------"<<endl;
   cout<<"Write output to root file"<<endl;
   TFile* fout = new TFile(outFile,"recreate");
-  h1d_kaon->Write();
-  h1d_pion->Write();
+  h2d_kaon->Write();
+  h2d_pion->Write();
   h1d_proton->Write();
   fout->Write();
   fout->Close();
--- multiplicity/analysis/eAu_18_110/plot_histogram.C
+++ multiplicity/analysis/eAu_18_110/plot_histogram.C
@@ -7,61 +7,68 @@ void plot_histogram(const char* inFile)
   cout<<"Print input file content"<<endl;
   fin->ls();
 
-  TH1D* h1d_kaon = (TH1D*)fin->Get("h1d_kaon");
-  TH1D* h1d_pion = (TH1D*)fin->Get("h1d_pion");
+  TH2D* h2d_kaon = (TH2D*)fin->Get("h2d_kaon");
+  TH1D* h1d_kaon_pos = (TH1D*) h2d_kaon->ProjectionX("h1d_kaon_pos");
+  TH1D* h1d_kaon_neg = (TH1D*) h2d_kaon->ProjectionY("h1d_kaon_neg");
+  TH1D* h1d_kaon_total = h1d_kaon_pos + h1d_kaon_neg;
+  TH2D* h2d_pion = (TH2D*)fin->Get("h2d_pion");
+  TH1D* h1d_pion_pos = (TH1D*) h2d_pion->ProjectionX("h1d_pion_pos");
+  TH1D* h1d_pion_neg = (TH1D*) h2d_pion->ProjectionY("h1d_pion_neg");
+  TH1D* h1d_pion_total = h1d_pion_pos + h1d_pion_neg;
   TH1D* h1d_proton = (TH1D*)fin->Get("h1d_proton");
 
 
   // charged kaon multiplicity
 
+  plot_h2d(h2d_kaon, "positive kaon multiplicity (counts)", "negative kaon multiplicity (counts)", "kaon_mul_2d.pdf");
+  plot_h1d(h1d_kaon_total, "charged kaon multiplicity (counts)", "events", "kaon_mul_2d.pdf");
+
+
+  // charged pion multiplicity
+
+  plot_h2d(h2d_pion, "positive pion multiplicity (counts)", "negative pion multiplicity (counts)", "pion_mul_2d.pdf");
+  plot_h1d(h1d_pion_total, "charged pion multiplicity (counts)", "events", "pion_mul_2d.pdf");
+
+
+  // proton multiplicity
+
+  plot_h1d(h1d_proton, "proton multiplicity (counts)", "events", "proton_mul.pdf");
+
+
+}
+
+void plot_h1d(TH1D * h1d, const char * x, const char * y, const char * filename)
+{
   TCanvas* c1 = new TCanvas("c1","c1",800,800); // create new canvas
   c1->Range(0,0,1,1);
   c1->SetLeftMargin(0.15);
   c1->SetBottomMargin(0.1);
   //c1->SetLogy(); // set y axis to log scale
 
-  h1d_kaon->GetXaxis()->SetTitle("charge kaon multiplicity (counts)");
-  h1d_kaon->GetYaxis()->SetTitle("events");
-  h1d_kaon->GetXaxis()->SetTitleOffset(1.3);
-  h1d_kaon->GetYaxis()->SetTitleOffset(1.5);
+  h1d->GetXaxis()->SetTitle(x);
+  h1d->GetYaxis()->SetTitle(y);
+  h1d->GetXaxis()->SetTitleOffset(1.3);
+  h1d->GetYaxis()->SetTitleOffset(1.5);
 
-  h1d_kaon->Draw("hsame");
+  h1d->Draw("hsame");
 
-  c1->SaveAs("charged_kaon_mul.pdf");
+  c1->SaveAs(filename);
+}
 
-  // charged pion multiplicity
+void plot_h2d(TH1D * h2d, const char * x, const char * y, const char * filename)
+{
+  TCanvas* c1 = new TCanvas("c1","c1",800,800); // create new canvas
+  c1->Range(0,0,1,1);
+  c1->SetLeftMargin(0.15);
+  c1->SetBottomMargin(0.1);
+  //c1->SetLogy(); // set y axis to log scale
 
-  TCanvas* c2 = new TCanvas("c2","c2",800,800); // create new canvas
-  c2->Range(0,0,1,1);
-  c2->SetLeftMargin(0.15);
-  c2->SetBottomMargin(0.1);
-  //c2->SetLogy(); // set y axis to log scale
+  h2d->GetXaxis()->SetTitle(x);
+  h2d->GetYaxis()->SetTitle(y);
+  h2d->GetXaxis()->SetTitleOffset(1.3);
+  h2d->GetYaxis()->SetTitleOffset(1.5);
 
-  h1d_pion->GetXaxis()->SetTitle("charge pion multiplicity (counts)");
-  h1d_pion->GetYaxis()->SetTitle("events");
-  h1d_pion->GetXaxis()->SetTitleOffset(1.3);
-  h1d_pion->GetYaxis()->SetTitleOffset(1.5);
+  h2d->Draw("colz");
 
-  h1d_pion->Draw("hsame");
-
-  c2->SaveAs("charged_pion_mul.pdf");
-
-  // charged kaon multiplicity
-
-  TCanvas* c3 = new TCanvas("c3","c3",800,800); // create new canvas
-  c3->Range(0,0,1,1);
-  c3->SetLeftMargin(0.15);
-  c3->SetBottomMargin(0.1);
-  //c3->SetLogy(); // set y axis to log scale
-
-  h1d_proton->GetXaxis()->SetTitle("proton multiplicity (counts)");
-  h1d_proton->GetYaxis()->SetTitle("events");
-  h1d_proton->GetXaxis()->SetTitleOffset(1.3);
-  h1d_proton->GetYaxis()->SetTitleOffset(1.5);
-
-  h1d_proton->Draw("hsame");
-
-  c3->SaveAs("proton_mul.pdf");
-
-
+  c1->SaveAs(filename);
 }
