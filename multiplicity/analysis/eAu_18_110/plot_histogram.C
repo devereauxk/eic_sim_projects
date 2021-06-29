482303d3ef7158d23623e6199d6fc03019cff053
unknown <devereauxk@berkeley.edu>
2021-06-29 10:24:54 -0700

    multiplicity mod

--- multiplicity/analysis/eAu_18_110/plot_histogram.C
+++ multiplicity/analysis/eAu_18_110/plot_histogram.C
@@ -10,13 +10,13 @@ void plot_histogram(const char* inFile)
   TH2D* h2d_kaon = (TH2D*)fin->Get("h2d_kaon");
   TH1D* h1d_kaon_pos = (TH1D*) h2d_kaon->ProjectionX("h1d_kaon_pos");
   TH1D* h1d_kaon_neg = (TH1D*) h2d_kaon->ProjectionY("h1d_kaon_neg");
-  TH1D* h1d_kaon_total = new TH1D("h1d_kaon_total", "charged kaon multiplicity", 100, 0, 12, 0, 12);
+  TH1D* h1d_kaon_total = new TH1D("h1d_kaon_total", "charged kaon multiplicity", 100, 0, 12);
   h1d_kaon_total->Add(h1d_kaon_pos, h1d_kaon_neg);
 
   TH2D* h2d_pion = (TH2D*)fin->Get("h2d_pion");
   TH1D* h1d_pion_pos = (TH1D*) h2d_pion->ProjectionX("h1d_pion_pos");
   TH1D* h1d_pion_neg = (TH1D*) h2d_pion->ProjectionY("h1d_pion_neg");
-  TH1D* h1d_pion_total = new TH1D("h1d_pion_total", "charged pion multiplicity", 100, 0, 10000, 0, 10000);
+  TH1D* h1d_pion_total = new TH1D("h1d_pion_total", "charged pion multiplicity", 100, 0, 10000);
   h1d_pion_total->Add(h1d_pion_pos, h1d_pion_neg);
 
   TH1D* h1d_proton = (TH1D*)fin->Get("h1d_proton");
