3ff09fef92d783d73750680deb1a73894d6f5df8
unknown <devereauxk@berkeley.edu>
2021-07-08 21:28:43 -0700

    condor mod

--- multiplicity/condor/eAu_18_110/make_tree.C
+++ multiplicity/condor/eAu_18_110/make_tree.C
@@ -4,7 +4,8 @@ void make_tree(std::string filstr){
 
   //erhic::DisKinematics::BoundaryWarning=false; //Need to comment this for eic account
 
-  std::string inputstr = filstr;
-  std::string outputdir = inputstr;
+  std::string dirstr = ".";
+  std::string inputstr = dirstr + "/" + filstr;
+  std::string outputdir = dirstr;
   BuildTree(inputstr,outputdir);
 }
