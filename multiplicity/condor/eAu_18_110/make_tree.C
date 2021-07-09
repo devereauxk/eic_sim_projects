18da7279c325d3d3fef4b08d3a956f45bc4c9e72
unknown <devereauxk@berkeley.edu>
2021-07-08 19:03:38 -0700

    condor mod

--- multiplicity/condor/eAu_18_110/make_tree.C
+++ multiplicity/condor/eAu_18_110/make_tree.C
@@ -3,8 +3,8 @@ R__LOAD_LIBRARY(libeicsmear)
 void make_tree(std::string filstr){
 
   //erhic::DisKinematics::BoundaryWarning=false; //Need to comment this for eic account
-  
-  std::string dirstr = "outForPythiaMode";
+
+  std::string dirstr = "";
   std::string inputstr = dirstr + "/" + filstr;
   std::string outputdir = dirstr;
   BuildTree(inputstr,outputdir);
