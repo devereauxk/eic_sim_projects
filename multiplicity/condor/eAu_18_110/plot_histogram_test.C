da11eb6c88ad7f5e7a37d4604174ad6f1fe5abaf
unknown <devereauxk@berkeley.edu>
2021-07-11 17:35:16 -0700

    condor eD

---
+++ multiplicity/condor/eAu_18_110/plot_histogram_test.C
@@ -0,0 +1,80 @@
+void insert_text(TH1D * h1d)
+{
+  TLatex* tl = new TLatex();
+  tl->SetTextAlign(11);
+  tl->SetTextSize(0.035);
+  tl->SetTextColor(kBlack);
+  tl->DrawLatexNDC(0.2,0.85,"e+Au @ 18GeV, 110GeV");
+  tl->SetTextColor(kBlue);
+  tl->DrawLatexNDC(0.2,0.80,Form("Total counts on plot is %.3e",h1d->GetEntries()));
+}
+
+void plot_histogram_test(const char* inFile)
+{
+  // run with root -l 'plot_histogram("histogram_dir/access_tree_output.root", "histogram_dir/")'
+  // p_T distribution
+
+  TFile* fin = new TFile(inFile,"read");
+  cout<<"Print input file content"<<endl;
+  fin->ls();
+
+  TH2D* h2d_kaon = (TH2D*)fin->Get("h2d_kaon");
+  TH1D* h1d_kaon_pos = (TH1D*) h2d_kaon->ProjectionX("h1d_kaon_pos");
+  TH1D* h1d_kaon_neg = (TH1D*) h2d_kaon->ProjectionY("h1d_kaon_neg");
+  TH1D* h1d_kaon_total = (TH1D*)fin->Get("h1d_kaon_total");
+
+  TH2D* h2d_pion = (TH2D*)fin->Get("h2d_pion");
+  TH1D* h1d_pion_pos = (TH1D*) h2d_pion->ProjectionX("h1d_pion_pos");
+  TH1D* h1d_pion_neg = (TH1D*) h2d_pion->ProjectionY("h1d_pion_neg");
+  TH1D* h1d_pion_total = (TH1D*)fin->Get("h1d_pion_total");
+
+  TH2D* h2d_proton = (TH2D*)fin->Get("h2d_proton");
+  TH1D* h1d_proton = (TH1D*) h2d_proton->ProjectionX("h1d_proton");
+  TH1D* h1d_anti_proton = (TH1D*) h2d_proton->ProjectionY("h1d_anti_proton");
+  TH1D* h1d_proton_total = (TH1D*)fin->Get("h1d_proton_total");
+
+  TCanvas * c_all = new TCanvas("c_all", "c_all", 2400, 1600);
+  c_all->Divide(3, 2);
+
+  // proton multiplicity
+
+  c_all->cd(1);
+  h1d_proton_total->GetXaxis()->SetTitle("proton+antiproton multiplicity [counts]");
+  h1d_proton_total->GetYaxis()->SetTitle("fraction of events [%]");
+  h1d_proton_total->Scale(1 / h1d_proton_total->GetEntries());
+  h1d_proton_total->GetXaxis()->SetTitleOffset(1.3);
+  h1d_proton_total->GetYaxis()->SetTitleOffset(1.5);
+  h1d_proton_total->Draw("hsame");
+  insert_text(h1d_proton_total);
+
+  c_all->cd(4);
+  TH2F htemp("htemp","",12,-0.5,11.5,10,0,1.5*h1d_proton->GetMaximum());
+  htemp.Draw();
+  htemp.GetXaxis()->SetTitle("M [counts]");
+  htemp.GetYaxis()->SetTitle("fraction of events [%]");
+  htemp.Scale(1 / h1d_proton->GetEntries());
+  htemp.GetXaxis()->SetTitleOffset(1.3);
+  htemp.GetYaxis()->SetTitleOffset(1.5);
+
+  h1d_proton->SetLineColor(kRed);
+  h1d_proton->Draw("hsame");
+
+  h1d_anti_proton->SetLineColor(kBlue);
+  h1d_anti_proton->Draw("hsame");
+
+  c_all->SaveAs("temp.pdf");
+
+
+}
+
+
+/*
+THStack *hs = new THStack("hs", "");
+hs->Add(h1d_kaon_pos);
+hs->Add(h1d_kaon_neg);
+TCanvas *cs = new TCanvas("cs", "cs", 1400, 700);
+cs->Divide(2);
+cs->cd(1); h2d_kaon->Draw("colz");
+cs->cd(2); hs->Draw("nostackb");
+cs->SaveAs("comp_kaon_mul.pdf");
+*/
