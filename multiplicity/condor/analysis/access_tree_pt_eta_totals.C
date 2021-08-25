6b9534cbe5a234dfdf0537cfda3771d792ead2ea
unknown <devereauxk@berkeley.edu>
2021-08-24 18:51:38 -0700

    solidified changes to pt_eta_totals and pt_eta_decomp, no longer in debugging

--- multiplicity/condor/analysis/access_tree_pt_eta_totals.C
+++ multiplicity/condor/analysis/access_tree_pt_eta_totals.C
@@ -14,6 +14,7 @@ void access_tree_pt_eta_totals()
 
   const int num_species = 7;
   std::string dirs[num_species] = {"../ep_10_100/outfiles/", "../eD_18_110/outForPythiaMode/",  "../eHe4_18_110/outForPythiaMode/", "../eC_18_110/outForPythiaMode/", "../eCa_18_110/outForPythiaMode/", "../eCu_18_110/outForPythiaMode/", "../eAu_18_110/outForPythiaMode/"};
+  std::string inFileNames[num_species] = {"merged.root", "merged.root", "merged.root", "merged.root", "merged.root", "merged.root", "merged.root"};
 
   TH2D * h2d_pt_vs_eta;
 
