b094f8718b68aa788f6b01716d65f8933815b0cb
unknown <devereauxk@berkeley.edu>
2021-09-09 02:30:55 -0700

    adding x analysis

---
+++ multiplicity/condor/analysis/access_tree_x_Q2_totals.C
@@ -0,0 +1,79 @@
+R__LOAD_LIBRARY(libeicsmear);
+void access_tree_x_Q2_totals()
+{
+  // run with root -l 'access_tree("sim_dir/sim_output.root", "histogram_dir/output.root")'
+  // arg: inFile: directory + filename of .root file outputed from sim
+
+  // makes num_species amount of 2D histograms counting multiplicity wrt pT and eta
+
+  //Event Class
+  erhic::EventPythia *event(NULL); //Note that I use Pointer
+
+  //Particle Class
+  erhic::ParticleMC *particle(NULL); //Also use Pointer
+
+  const int num_species = 7;
+  std::string dirs[num_species] = {"../eD_18_110/outForPythiaMode/",  "../eHe4_18_110/outForPythiaMode/", "../eC_18_110/outForPythiaMode/", "../eCa_18_110/outForPythiaMode/", "../eCu_18_110/outForPythiaMode/", "../eAu_18_110/outForPythiaMode/", "../ep_10_100/outfiles/"};
+  std::string inFileNames[num_species] = {"merged.root", "merged.root", "merged.root", "merged.root", "merged.root", "merged.root", "merged.root"};
+
+  TH2D * h2d_x_vs_q2;
+
+  //loop over files
+  TFile *f;
+  TFile *fout;
+  std::string inFile;
+  std::string outFile;
+  TTree *tree;
+  Int_t nEntries;
+  Int_t nParticles(0);
+  Int_t id;
+  Int_t status;
+  Double_t x;
+  Double_t q2;
+
+  //loop over each merged.root file
+  for (int i = 0; i < num_species; i++) {
+    cout<<dirs[i]<<endl;
+    //Load ROOT File for pythia
+    inFile = dirs[i] + inFileNames[i];
+    f = new TFile(inFile.c_str(), "read");
+
+    //Get EICTree Tree
+    tree = (TTree*)f->Get("EICTree");
+    nEntries = tree->GetEntries();
+
+    //Access event Branch
+    tree->SetBranchAddress("event",&event); //Note &event, even with event being a pointer
+
+    //load new out file
+    outFile = dirs[i] + "x_Q2_total_TH2D.root";
+    fout = new TFile(outFile.c_str(), "recreate");
+
+    //Initialize new histogram
+    h2d_x_vs_q2 = new TH2D("h2d_x_vs_q2","event x vs Q^2",100,1/100000,1,100,10,100);
+    h2d_x_vs_q2->Sumw2();
+
+    //loop over events
+    for (int j = 0; j < nEntries; j++) {
+
+      tree->GetEntry(j);
+
+      //Get Total Number of Particles
+      nParticles = event->GetNTracks();
+
+      x = event->GetX();
+      q2 = event->GetQ2();
+
+      if (event->GetProcess() == 99) {
+        h2d_x_vs_q2->Fill(x, q2);
+      }
+
+    }
+
+    h2d_x_vs_q2->Write();
+    fout->Write();
+    fout->Close();
+
+  }
+
+}
---
+++ multiplicity/condor/analysis/plot_histograms_x_Q2_totals.C
@@ -0,0 +1,41 @@
+TH2D* h2d_x_vs_q2 = NULL;
+
+const int num_species = 7;
+std::string dirs[num_species] = {"../ep_10_100/outfiles/", "../eD_18_110/outForPythiaMode/",  "../eHe4_18_110/outForPythiaMode/", "../eC_18_110/outForPythiaMode/", "../eCa_18_110/outForPythiaMode/", "../eCu_18_110/outForPythiaMode/", "../eAu_18_110/outForPythiaMode/"};
+std::string names[num_species] = {"e + p @ 10 + 100 GeV", "e + D @ 18 + 110 GeV", "e + He4 @ 18 + 110 GeV", "e + C @ 18 + 110 GeV", "e + Ca @ 18 + 110 GeV", "e + Cu @ 18 + 110 GeV", "e + Au @ 18 + 110 GeV"};
+std::string outdirs[num_species] = {"../ep_10_100/", "../eD_18_110/", "../eHe4_18_110/", "../eC_18_110/", "../eCa_18_110/", "../eCu_18_110/", "../eAu_18_110/"};
+std::string inFileName = "x_Q2_total_TH2D.root";
+
+
+void plot_histograms_x_Q2_totals()
+{
+  TFile * fin;
+  std::string inFile;
+  for (int i = 0; i < num_species; i++) {
+    inFile = dirs[i] + inFileName;
+    fin = new TFile(inFile.c_str(), "read");
+    fin->ls();
+    h2d_x_vs_q2 = (TH2D*)fin->Get("h2d_x_vs_q2");
+
+    // plots pt and eta 2d histogram
+    TCanvas* c3 = new TCanvas("c3","c3",800,800); // create new canvas
+    c3->Range(0,0,1,1);
+    c3->SetLeftMargin(0.15);
+    c3->SetRightMargin(0.15);
+    c3->SetBottomMargin(0.1);
+    h2d_x_vs_q2 = (TH2D*) h2d_x_vs_q2->Rebin2D(2, 2);
+    h2d_x_vs_q2->SetTitle(names[i].c_str());
+    h2d_x_vs_q2->GetXaxis()->SetTitle("x");
+    h2d_x_vs_q2->GetYaxis()->SetTitle("Q^{2}");
+    h2d_x_vs_q2->GetXaxis()->SetTitleOffset(1.3);
+    h2d_x_vs_q2->GetYaxis()->SetTitleOffset(1.5);
+    c3->SetLogx();
+    c3->SetLogy();
+    c3->SetLogz();
+    h2d_x_vs_q2->Draw("colz");
+    h2d_x_vs_q2->SetStats(0);
+    c3->SaveAs(Form("%sx_vs_Q2_2D_histo.pdf", outdirs[i].c_str()));
+
+  }
+
+}
