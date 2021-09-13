a7f79566962e514e3b370b278f678032dd27e541
unknown <devereauxk@berkeley.edu>
2021-09-13 10:34:37 -0700

    changes to x analysis

--- multiplicity/condor/analysis/access_tree_mult_x_binned.C
+++ multiplicity/condor/analysis/access_tree_mult_x_binned.C
@@ -88,8 +88,9 @@ void access_tree_mult_x_binned()
 
       if (event->GetProcess() == 99) {
         for (Int_t ix = 0; ix < xbin; ix++) {
-          cout<<"runs"<<endl;
+          cout<<x_lo[ix]<<" "<<x<<" "<<x_hi[ix]<<endl;    // runs
           if (x_lo[ix] <= x && x <= x_hi[ix]) {
+            cout<<"runs"<<endl;  // doesn't run
 
             //Loop Over Each Particle
             for(Int_t k = 0; k < nParticles; k++) {
