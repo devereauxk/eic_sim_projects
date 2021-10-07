efcc8a5e9293dbb81f52481ae25a1ca95270ac19
unknown <devereauxk@berkeley.edu>
2021-10-07 16:03:27 -0700

    histogram plotter modded

--- reconstruction/plot_histogram.C
+++ reconstruction/plot_histogram.C
@@ -9,14 +9,18 @@ void plot_histogram(const char* inFile, const char* outDir)
   c->SetRightMargin(0.15);
   c->SetBottomMargin(0.1);
 
-  TH1D* x = (TH1D*) ((TH2D*) f->Get("fg2d_Kpimass_vs_p_2_0"))->ProjectionX("x");
-  x->Draw("hsame");
-  x->GetXaxis()->SetRangeUser(1.7,2);
-  c->SaveAs(Form("%sfg2d_Kpimass_vs_p_2_0_proj.pdf", outDir));
+  for (int ieta = 0; ieta < 3; ieta++) {
 
-  TH2D* x2 = (TH2D*) f->Get("fg2d_Kpimass_vs_p_2_0");
-  x2->Draw("colz");
-  c->SetLogz();
-  c->SaveAs(Form("%sfg2d_Kpimass_vs_p_2_0.pdf", outDir));
+    TH1D* x = (TH1D*) ((TH2D*) f->Get(Form("fg2d_Kpimass_vs_p_2_%d", ieta)))->ProjectionX("x");
+    x->Draw("hsame");
+    x->GetXaxis()->SetRangeUser(1.7,2);
+    c->SaveAs(Form("%sfg2d_Kpimass_vs_p_2_%d_proj.pdf", outDir, ieta));
+
+    TH2D* x2 = (TH2D*) f->Get(Form("fg2d_Kpimass_vs_p_2_%d", ieta));
+    x2->Draw("colz");
+    c->SetLogz();
+    c->SaveAs(Form("%sfg2d_Kpimass_vs_p_2_%d.pdf", outDir, ieta));
+
+  }
 
 }
