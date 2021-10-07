15d12cad4fb338beca6ef96b977b2d4491e60239
unknown <devereauxk@berkeley.edu>
2021-10-07 15:58:13 -0700

    histogram plotter modded

--- reconstruction/plot_histogram.C
+++ reconstruction/plot_histogram.C
@@ -9,9 +9,9 @@ void plot_histogram(const char* inFile, const char* outDir)
   c->SetRightMargin(0.15);
   c->SetBottomMargin(0.1);
 
-  TH1D* x = (TH1D*) f->Get("fg2d_Kpimass_vs_p_2_0")->ProjectionX("x")
+  TH1D* x = (TH1D*) ((TH2D*) f->Get("fg2d_Kpimass_vs_p_2_0"))->ProjectionX("x")
   x->Draw("hsame");
-  x->GetXaxis()->SetRangeUser(1.7,2)
+  x->GetXaxis()->SetRangeUser(1.7,2);
   c->SaveAs(Form("%sfg2d_Kpimass_vs_p_2_0_proj.pdf", outDir));
 
   TH2D* x2 = (TH2D*) f->Get("fg2d_Kpimass_vs_p_2_0");
