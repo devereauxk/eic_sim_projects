8dd4781d9f064e3f2556531a782f56a86d263a02
unknown <devereauxk@berkeley.edu>
2021-10-08 18:03:16 -0700

    hist mods

--- reconstruction/plot_histogram.C
+++ reconstruction/plot_histogram.C
@@ -20,22 +20,27 @@ void plot_histogram(const char* inFile, const char* outDir)
     x->Draw("hsame");
     x->GetXaxis()->SetRangeUser(1.7,2);
     x->SetTitle("");
-    x->GetXaxis()->SetTitle("M(K^{#pm}#pi^{-#pm}) [GeV/c^{2}]");
+    x->GetXaxis()->SetTitle("M(K^{#pm}#pi^{#mp}) [GeV/c^{2}]");
     x->GetYaxis()->SetTitle("counts");
     x->GetXaxis()->SetTitleOffset(1.3);
     x->GetYaxis()->SetTitleOffset(1.5);
     x->SetStats(0);
+    TLatex* tl = new TLatex();
+    tl->SetTextAlign(11);
+    tl->SetTextSize(0.035);
+    tl->SetTextColor(kBlack);
+    tl->DrawLatexNDC(0.6,0.85,Form("%.0f < #eta < %.0f",eta_lo[ieta],eta_hi[ieta]));
     c->SaveAs(Form("%sfg2d_Kpimass_2_%d.pdf", outDir, ieta));
 
     TH2D* x2 = (TH2D*) f->Get(Form("fg2d_Kpimass_vs_p_2_%d", ieta));
     x2->Draw("colz");
     c->SetLogz();
     x->SetTitle("");
-    x->GetXaxis()->SetTitle("M(K^{#pm}#pi^{-#pm}) [GeV/c^{2}]");
-    x->GetYaxis()->SetTitle("<p_{T}>(K^{#pm}#pi^{-#pm}) [GeV/c]");
-    x->GetZaxis()->SetTitle("counts");
+    x->GetXaxis()->SetTitle("M(K^{#pm}#pi^{#mp}) [GeV/c^{2}]");
+    x->GetYaxis()->SetTitle("<p_{T}>(K^{#pm}#pi^{#mp}) [GeV/c]");
     x->GetXaxis()->SetTitleOffset(1.3);
     x->GetYaxis()->SetTitleOffset(1.5);
+    x->SetStats(0);
     c->SaveAs(Form("%sfg2d_Kpimass_vs_p_2_%d.pdf", outDir, ieta));
 
   }
