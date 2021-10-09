0d1310ae93b35d87e9a86768665f55a0d0f39288
unknown <devereauxk@berkeley.edu>
2021-10-08 17:45:26 -0700

    hist mods

--- reconstruction/plot_histogram.C
+++ reconstruction/plot_histogram.C
@@ -1,3 +1,7 @@
+const int etabin = 3;
+static double eta_lo[etabin] = {-3,-1,1};
+static double eta_hi[etabin] = {-1,1,3};
+
 void plot_histogram(const char* inFile, const char* outDir)
 {
 
@@ -15,11 +19,23 @@ void plot_histogram(const char* inFile, const char* outDir)
     TH1D* x = (TH1D*) ((TH2D*) f->Get(Form("fg2d_Kpimass_vs_p_2_%d", ieta)))->ProjectionX("x");
     x->Draw("hsame");
     x->GetXaxis()->SetRangeUser(1.7,2);
-    c->SaveAs(Form("%sfg2d_Kpimass_vs_p_2_%d_proj.pdf", outDir, ieta));
+    x->SetTitle("");
+    x->GetXaxis()->SetTitle("M(K^{#pm}#pi^{-#pm}) [GeV/c^{2}]");
+    x->GetYaxis()->SetTitle("counts");
+    x->GetXaxis()->SetTitleOffset(1.3);
+    x->GetYaxis()->SetTitleOffset(1.5);
+    x->SetStats(0);
+    c->SaveAs(Form("%sfg2d_Kpimass_2_%d.pdf", outDir, ieta));
 
     TH2D* x2 = (TH2D*) f->Get(Form("fg2d_Kpimass_vs_p_2_%d", ieta));
     x2->Draw("colz");
     c->SetLogz();
+    x->SetTitle("");
+    x->GetXaxis()->SetTitle("M(K^{#pm}#pi^{-#pm}) [GeV/c^{2}]");
+    x->GetYaxis()->SetTitle("<p_{T}>(K^{#pm}#pi^{-#pm}) [GeV/c]");
+    x->GetZaxis()->SetTitle("counts")
+    x->GetXaxis()->SetTitleOffset(1.3);
+    x->GetYaxis()->SetTitleOffset(1.5);
     c->SaveAs(Form("%sfg2d_Kpimass_vs_p_2_%d.pdf", outDir, ieta));
 
   }
