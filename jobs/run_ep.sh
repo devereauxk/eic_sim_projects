314439cd46ab4cb7af2b552bd6f06fa9a10207a7
unknown <devereauxk@berkeley.edu>
2021-10-15 17:22:37 -0700

    eAu and ep folders added to reconstruction for 1E7 event data

--- jobs/run_ep.sh
+++ jobs/run_ep.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 #-----#-----#----#----#----#----#----#----#----#
 
+WORKING_DIR=/eic/u/kdevereaux/work/reconstruction/eAu_18_110
+
 if [ -z "$1" ]
 then
         echo "No job number set."
@@ -31,8 +33,8 @@ source /cvmfs/sphenix.opensciencegrid.org/gcc-8.3/opt/sphenix/core/gcc/8.3.0.1-0
 #ln -s /eic/data/baraks/BeAGLE/inputFiles/S3ALL003
 #ln -s /eic/data/baraks/BeAGLE/nuclear.bin
 #ln -s /eic/data/baraks/BeAGLE/make_tree.C
-ln -s /eic/u/kdevereaux/work/multiplicity/condor/ep_10_100/input.data_noradcor.eic
-ln -s /eic/u/kdevereaux/work/multiplicity/condor/ep_10_100/make_tree.C
+ln -s ${WORKING_DIR}/input.data_noradcor.eic
+ln -s ${WORKING_DIR}/make_tree.C
 
 
 #Run simulation
@@ -54,6 +56,6 @@ echo "Cleaning Up..."
 #mv -v eA.txt /eic/data/baraks/BeAGLE/outForPythiaMode/5_41/eAu/eAu_${INPUT}.txt
 #mv -v eA.root /eic/data/baraks/BeAGLE/outForPythiaMode/5_41/eAu/eAu_${INPUT}.root
 #mv -v eAu.log /eic/data/baraks/BeAGLE/logs/5_41/eAu/eAu_${INPUT}.log
-mv -v ep_10_100_norad.root /eic/u/kdevereaux/work/multiplicity/condor/ep_10_100/outfiles/ep_${INPUT}.root
-mv -v ep_10_100_norad.log /eic/u/kdevereaux/work/multiplicity/condor/ep_10_100/logfiles/ep_${INPUT}.log
+mv -v ep_10_100_norad.root ${WORKING_DIR}/outfiles/ep_${INPUT}.root
+mv -v ep_10_100_norad.log ${WORKING_DIR}/logfiles/ep_${INPUT}.log
 echo "DONE!!!"
