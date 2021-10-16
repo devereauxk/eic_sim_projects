0bfebaff18585dd74762e6b2d49e522ae22c1131
unknown <devereauxk@berkeley.edu>
2021-10-15 19:49:42 -0700

    mistake with run_chadron and ep wenqing param passing, fixing, rerunning...

--- reconstruction/run_chadron.sh
+++ reconstruction/run_chadron.sh
@@ -20,8 +20,9 @@ NEVT=$(( $2 ))
 SMEAR_OPT=$3 # 0--no smearing, 1--DM smearing, 2--LBL smearing, 3--Hybrid smearing
 BFIELD=$4 # 0--Barbar, 1--Beast
 ID_OPT=$5 # 0--no hID (but with eID), 1--PID with no low momentum cutoff, 2--PID with low momentum cutoff & some mis-identified pi, K, 3--PID with low momentum cutoff & all identified pi, K
+DCA=$6 # DCA_cut: 0--no cut, 1--cut on DCA
 
-mkdir /gpfs/mnt/gpfs02/eic/kdevereaux/reconstruction/hist_output/S${SMEAR_OPT}_B${BFIELD}_ID${ID_OPT}
+mkdir /gpfs/mnt/gpfs02/eic/kdevereaux/reconstruction/hist_output/S${SMEAR_OPT}_B${BFIELD}_ID${ID_OPT}_DCA${DCA}
 
 ln -s /gpfs/mnt/gpfs02/eic/kdevereaux/reconstruction/fast_sim.h .
 ln -s /gpfs/mnt/gpfs02/eic/kdevereaux/reconstruction/D0_tree.C .
@@ -38,13 +39,13 @@ do
     hname=`echo hists-$fno.root`
     echo histogram name is $hname
 
-    if [ -a /gpfs/mnt/gpfs02/eic/kdevereaux/reconstruction/hist_output/S${SMEAR_OPT}_B${BFIELD}_ID${ID_OPT}/$hname ]
+    if [ -a /gpfs/mnt/gpfs02/eic/kdevereaux/reconstruction/hist_output/S${SMEAR_OPT}_B${BFIELD}_ID${ID_OPT}_DCA${DCA}/$hname ]
     then
       echo "File already exists"
     else
-      root -b -q 'D0_tree.C("'$file'","'$hname'",'$NEVT','$SMEAR_OPT','$BFIELD','$ID_OPT')'
+      root -b -q 'D0_tree.C("'$file'","'$hname'",'$NEVT','$SMEAR_OPT','$BFIELD','$ID_OPT','$DCA')'
       echo "job done...move output file: ${hname}"
-      mv $hname /gpfs/mnt/gpfs02/eic/kdevereaux/reconstruction/hist_output/S${SMEAR_OPT}_B${BFIELD}_ID${ID_OPT}
+      mv $hname /gpfs/mnt/gpfs02/eic/kdevereaux/reconstruction/hist_output/S${SMEAR_OPT}_B${BFIELD}_ID${ID_OPT}_DCA${DCA}
     fi
   fi
   NUM=$(( $NUM + 1 ))
