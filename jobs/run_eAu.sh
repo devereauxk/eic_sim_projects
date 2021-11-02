02baf928519825cc970f3c3926ea597f78cde6ee
unknown <devereauxk@berkeley.edu>
2021-11-01 17:02:03 -0700

    new beagle version accounted for

--- jobs/run_eAu.sh
+++ jobs/run_eAu.sh
@@ -23,7 +23,8 @@ mkdir $DIR
 cd $DIR
 
 #Environmental Variables
-export BEAGLESYS="${EICDIRECTORY}/PACKAGES/BeAGLE"
+#export BEAGLESYS="${EICDIRECTORY}/PACKAGES/BeAGLE" # manual overide
+export BEAGLESYS=/afs/rhic.bnl.gov/eic/PACKAGES/BeAGLE
 export LHAPDF5="${EICDIRECTORY}/gcc-8.3/lib"
 export LD_LIBRARY_PATH="${LHAPDF5}:$LD_LIBRARY_PATH"
 source /cvmfs/sphenix.opensciencegrid.org/gcc-8.3/opt/sphenix/core/gcc/8.3.0.1-0a5ad/x86_64-centos7/setup.sh
