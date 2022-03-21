d83e84088cc51573db269900417a63dcc42b1af2
Kyle Devereaux <devereauxk@berkeley.edu>
2022-03-21 15:44:22 -0700

    plot_histogram signal extraction update

--- reconstruction/plot_histogram.C
+++ reconstruction/plot_histogram.C
@@ -26,8 +26,8 @@ void plot_histogram(const char* inFile, const char* outDir, const char* title = 
 
     TH1D* fg1d_Kpimass_vs_p = (TH1D*) ((TH2D*) f->Get(Form("fg2d_Kpimass_vs_p_2_%d", ieta)))->ProjectionX("x");
     TH1D* bg1d_Kpimass_vs_p = (TH1D*) ((TH2D*) f->Get(Form("bg2d_Kpimass_vs_p_2_%d", ieta)))->ProjectionX("x");
-    TH1D* sg1d_Kpimass_vs_p = (TH1D*) ((TH2D*) f->Get(Form("fg2d_Kpimass_vs_p_2_%d", ieta)))->ProjectionX("x");
-    //sg1d_Kpimass_vs_p->Add(bg1d_Kpimass_vs_p, -1);
+    TH1D* sg1d_Kpimass_vs_p = (TH1D*) fg1d_Kpimass_vs_p->Clone();
+    sg1d_Kpimass_vs_p->Add(bg1d_Kpimass_vs_p, -1);
 
     float temp_mean = -9999;
     float temp_sigma = 0;
