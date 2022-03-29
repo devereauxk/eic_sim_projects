249fec0e058c9d49cb36efa30f325319bf6f3477
Kyle Devereaux <devereauxk@berkeley.edu>
2022-03-28 17:40:38 -0700

    try

--- reconstruction/plot_histogram.C
+++ reconstruction/plot_histogram.C
@@ -86,26 +86,26 @@ void plot_histogram(const char* inFile, const char* outDir, const char* title = 
       //Lc
       TH1D* fg1d_Kpipmass_vs_p = (TH1D*) ((TH2D*) f->Get(Form("fg2d_Kpipmass_vs_p_2_%d", ieta)))->ProjectionX("fg1d_Kpip");
       TH1D* bg1d_Kpipmass_vs_p = (TH1D*) ((TH2D*) f->Get(Form("bg2d_Kpipmass_vs_p_2_%d", ieta)))->ProjectionX("bg1d_Kpip");
-      TH1D* sg1d_Kpipmass_vs_p = (TH1D*) fg1d_Kpimass_vs_p->Clone();
+      TH1D* sg1d_Kpipmass_vs_p = (TH1D*) fg1d_Kpipmass_vs_p->Clone();
       sg1d_Kpipmass_vs_p->SetName("sg1d_Kpip");
       sg1d_Kpipmass_vs_p->Add(bg1d_Kpipmass_vs_p, -1);
 
-      plot_xrange_lo = 2.1;
-      plot_xrange_hi = 2.45;
-      plot_yrange_lo = 0;
-      plot_yrange_hi = 1.7*fg1d_Kpipmass_vs_p->GetMaximum();
+      float plot_xrange_lo = 2.1;
+      float plot_xrange_hi = 2.45;
+      float plot_yrange_lo = 0;
+      float plot_yrange_hi = 1.7*fg1d_Kpipmass_vs_p->GetMaximum();
 
-      TH2F htemp2("htemp","",10,plot_xrange_lo,plot_xrange_hi,10,plot_yrange_lo,plot_yrange_hi);
-      htemp2.Draw();
-      htemp2.GetXaxis()->SetTitle("m_{K^{#pm}#pi^{#mp}p^{#mp}} [GeV/c^{2}]");
-      htemp2.GetYaxis()->SetTitle("Counts");
-      myhset(&htemp2,1.2,1.6,0.05,0.045);
+      TH2F htemp("htemp","",10,plot_xrange_lo,plot_xrange_hi,10,plot_yrange_lo,plot_yrange_hi);
+      htemp.Draw();
+      htemp.GetXaxis()->SetTitle("m_{K^{#pm}#pi^{#mp}p^{#mp}} [GeV/c^{2}]");
+      htemp.GetYaxis()->SetTitle("Counts");
+      myhset(&htemp,1.2,1.6,0.05,0.045);
 
-      TLegend leg2(0.2,0.65,0.83,0.80);
-      leg2.SetBorderSize(0);
-      leg2.SetTextSize(0.03);
-      leg2.SetFillStyle(0);
-      leg2.SetMargin(0.1);
+      TLegend leg(0.2,0.65,0.83,0.80);
+      leg.SetBorderSize(0);
+      leg.SetTextSize(0.03);
+      leg.SetFillStyle(0);
+      leg.SetMargin(0.1);
 
       fg1d_Kpipmass_vs_p->SetLineColor(kBlue);
       bg1d_Kpipmass_vs_p->SetLineColor(kRed);
@@ -114,15 +114,15 @@ void plot_histogram(const char* inFile, const char* outDir, const char* title = 
       bg1d_Kpipmass_vs_p->Draw("hsame");
       sg1d_Kpipmass_vs_p->Draw("hsame");
 
-      temp_mean = -9999;
-      temp_sigma = 0;
+      float temp_mean = -9999;
+      float temp_sigma = 0;
       sg1d_Kpipmass_vs_p->Fit("gaus","0R","",2.1,2.4);
-      gaus = sg1d_Kpipmass_vs_p->GetFunction("gaus");
+      TF1* gaus = sg1d_Kpipmass_vs_p->GetFunction("gaus");
 
       if (gaus!=NULL)
       {
-        temp_mean = gaus->GetParameter(1);
-        temp_sigma = gaus->GetParameter(2);
+        float temp_mean = gaus->GetParameter(1);
+        float temp_sigma = gaus->GetParameter(2);
 
         TF1* func_peak = new TF1("func_peak","[0]+[1]*x+[2]*x*x+[3]*exp(-0.5*pow(x-[4],2)/pow([5],2))",temp_mean-3*temp_sigma,temp_mean+3*temp_sigma);
         //func_peak->SetLineColor(pid_color[ipid]);
@@ -132,12 +132,12 @@ void plot_histogram(const char* inFile, const char* outDir, const char* title = 
         fg1d_Kpipmass_vs_p->Fit(func_peak,"R","",temp_mean-8*temp_sigma,temp_mean+8*temp_sigma);
       }
 
-      int_range_lo = sg1d_Kpipmass_vs_p->FindBin(temp_mean-3*temp_sigma);
-      int_range_hi = sg1d_Kpipmass_vs_p->FindBin(temp_mean+3*temp_sigma);
-      N_SG = sg1d_Kpipmass_vs_p->Integral(int_range_lo, int_range_hi);
-      N_BG = bg1d_Kpipmass_vs_p->Integral(int_range_lo, int_range_hi);
+      float int_range_lo = sg1d_Kpipmass_vs_p->FindBin(temp_mean-3*temp_sigma);
+      float int_range_hi = sg1d_Kpipmass_vs_p->FindBin(temp_mean+3*temp_sigma);
+      float N_SG = sg1d_Kpipmass_vs_p->Integral(int_range_lo, int_range_hi);
+      float N_BG = bg1d_Kpipmass_vs_p->Integral(int_range_lo, int_range_hi);
 
-      tl = new TLatex();
+      TLatex* tl = new TLatex();
       tl->SetTextAlign(11);
       tl->SetTextSize(0.035);
       tl->SetTextColor(kBlack);
