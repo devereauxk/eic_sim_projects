1c7e1db45276313cb64a400b69cd9017dab39b1c
Kyle Devereaux <devereauxk@berkeley.edu>
2022-04-20 00:51:26 -0700

    temp

--- jobs/run_eAu.sh
+++ jobs/run_eAu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 #-----#-----#----#----#----#----#----#----#----#
 
-WORKING_DIR=/eic/u/kdevereaux/work/reconstruction/BeAGLE_characterization/eAu_10_100_true_taufor_2
+WORKING_DIR=/eic/u/kdevereaux/work/reconstruction/BeAGLE_characterization/eAu_10_100_true_taufor_05
 
 if [ -z "$1" ]
 then
