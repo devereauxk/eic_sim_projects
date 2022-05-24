cfb80c83fb99eecf6c7751dfce9f9ae8bc5dd51a
Kyle Devereaux <devereauxk@berkeley.edu>
2022-05-24 13:17:11 -0700

    try

--- reconstruction/BeAGLE_v102/run_plotter.sh
+++ reconstruction/BeAGLE_v102/run_plotter.sh
@@ -1,10 +1,10 @@
-EP_DIR=./BeAGLE_v101/ep_10_100_baseline_parp2/outfiles
-EA_DIR=./BeAGLE_v102/eAu_10_100/outForPythiaMode
+EP_DIR=./BeAGLE_v101/ep_10_100_baseline_parp2
+EA_DIR=./BeAGLE_v102/eAu_10_100
 
-root -l -q "ana_hadron_gen.C(\"'$EA_DIR'/merged.root\", \"'$EA_DIR'/ana_merged.root\")"
+#root -l -q "ana_hadron_gen.C(\"'$EA_DIR'/outForPythiaMode/merged.root\", \"'$EA_DIR'/outForPythiaMode/ana_merged.root\")"
 
-root -l -q "plot_chadron_gen.C(\"'$EP_DIR'/ana_merged.root\", 0, 1, \"'$EA_DIR'/ana_merged.root\", 1, 1)"
+#root -l -q "plot_chadron_gen.C(\"'$EP_DIR'/outfiles/ana_merged.root\", 0, 1, \"'$EA_DIR'/outForPythiaMode/ana_merged.root\", 1, 1)"
 
 cp topng.sh $EA_DIR/figs
-bash $EA_DIR/figs/topng.sh
-rm $EA_DIR/figs/topng.sh
+#bash $EA_DIR/figs/topng.sh
+#rm $EA_DIR/figs/topng.sh
