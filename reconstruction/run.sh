51e719fe66acc8022275c3eac125ea439f2375ff
Kyle Devereaux <devereauxk@berkeley.edu>
2022-05-26 17:57:39 -0400

    plots try

--- reconstruction/run.sh
+++ reconstruction/run.sh
@@ -8,14 +8,14 @@ EA_DIR=./BeAGLE_v102/eAu_10_100
 #echo "generating hadron histograms (writing to ana_merged.root file)..."
 #root -l -q "ana_hadron_gen.C(\"$EA_DIR/outForPythiaMode/merged.root\", \"$EA_DIR/outForPythiaMode/ana_merged.root\")"
 
-echo "generating histogram plots for eA (writing to figs/ folder)..."
-mkdir figs
-root -l -q "plot_chadron_gen.C(\"$EP_DIR/outfiles/ana_merged.root\", 0, 1, \"$EA_DIR/outForPythiaMode/ana_merged.root\", 1, 1, \"$EA_DIR/figs/hists_gen.root\")"
-mkdir $EA_DIR/figs
-mv figs/* $EA_DIR/figs
-rm -r figs
+#echo "generating histogram plots for eA (writing to figs/ folder)..."
+#mkdir figs
+#root -l -q "plot_chadron_gen.C(\"$EP_DIR/outfiles/ana_merged.root\", 0, 1, \"$EA_DIR/outForPythiaMode/ana_merged.root\", 1, 1, \"$EA_DIR/figs/hists_gen.root\")"
+#mkdir $EA_DIR/figs
+#mv figs/* $EA_DIR/figs
+#rm -r figs
 
-#echo "converting plot files from pdf to png..."
-#cp topng.sh $EA_DIR/figs
-#bash $EA_DIR/figs/topng.sh
-#rm $EA_DIR/figs/topng.sh
+echo "converting plot files from pdf to png..."
+cp topng.sh $EA_DIR/figs
+bash $EA_DIR/figs/topng.sh
+rm $EA_DIR/figs/topng.sh
