0c4515459014c31c608970810f43d11a66b2c418
Kyle Devereaux <devereauxk@berkeley.edu>
2022-05-31 20:15:55 -0700

    try

--- reconstruction/run.sh
+++ reconstruction/run.sh
@@ -1,21 +1,21 @@
 EP_DIR=./BeAGLE_v101/ep_10_100_baseline_parp2
-EA_DIR=./BeAGLE_v102/ep_10_100_BeAGLE
+EA_DIR=./BeAGLE_v102/eAu_10_100
 
 
-echo "merging root eA root files..."
-hadd -j $EA_DIR/outForPythiaMode/merged.root $EA_DIR/outForPythiaMode/*.root
+#echo "merging root eA root files..."
+#hadd -j $EA_DIR/outForPythiaMode/merged.root $EA_DIR/outForPythiaMode/*.root
 
 #echo "generating hadron histograms for ep (writing to ana_merged.root file)..."
 #root -l -q "ana_hadron_gen.C(\"$EP_DIR/outfiles/merged.root\", \"$EP_DIR/outfiles/ana_merged.root\", 0, 0, 0)"
 
-echo "generating hadron histograms for eA (writing to ana_merged.root file)..."
-root -l -q "ana_hadron_gen.C(\"$EA_DIR/outForPythiaMode/merged.root\", \"$EA_DIR/outForPythiaMode/ana_merged.root\", 0, 0, 1)"
+#echo "generating hadron histograms for eA (writing to ana_merged.root file)..."
+#root -l -q "ana_hadron_gen.C(\"$EA_DIR/outForPythiaMode/merged.root\", \"$EA_DIR/outForPythiaMode/ana_merged.root\", 0, 0, 1)"
 
 echo "generating histogram plots for ep/eA (writing to figs/ folder)..."
 mkdir figs
 root -l -q "plot_chadron_gen.C(\"$EP_DIR/outfiles/ana_merged.root\", 0, 1, \"$EA_DIR/outForPythiaMode/ana_merged.root\", 1, 1, \"$EA_DIR/figs/hists_gen.root\")"
-mkdir $EA_DIR/figs_norm_1
-mv figs/* $EA_DIR/figs_norm_1
+mkdir $EA_DIR/figs
+mv figs/* $EA_DIR/figs
 rm -r figs
 
 #echo "converting plot files from pdf to png..."
