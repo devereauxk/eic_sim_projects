407d862079085812ed9eaebd2de029ac29aed516
Kyle Devereaux <devereauxk@berkeley.edu>
2022-06-09 15:03:25 -0700

    eAu and eC runs

--- jobs/run_eC.sh
+++ jobs/run_eC.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 #-----#-----#----#----#----#----#----#----#----#
 
+WORKING_DIR=/eic/u/kdevereaux/work/reconstruction/BeAGLE_v102/eC_10_100_taufor05_qhat0_nlo
+
 if [ -z "$1" ]
 then
         echo "No job number set."
@@ -27,10 +29,10 @@ export LD_LIBRARY_PATH="${LHAPDF5}:$LD_LIBRARY_PATH"
 source /cvmfs/sphenix.opensciencegrid.org/gcc-8.3/opt/sphenix/core/gcc/8.3.0.1-0a5ad/x86_64-centos7/setup.sh
 
 #Soft links to necessary files
-ln -s /eic/u/kdevereaux/work/multiplicity/condor/eC_18_110/inputFiles/eC.inp
-ln -s /eic/u/kdevereaux/work/multiplicity/condor/eC_18_110/inputFiles/S3ALL003
-ln -s /eic/u/kdevereaux/work/multiplicity/condor/eC_18_110/nuclear.bin
-ln -s /eic/u/kdevereaux/work/multiplicity/condor/eC_18_110/make_tree.C
+ln -s ${WORKING_DIR}/inputFiles/eC.inp
+ln -s ${WORKING_DIR}/inputFiles/S1ALL003
+ln -s ${WORKING_DIR}/nuclear.bin
+ln -s ${WORKING_DIR}/make_tree.C
 
 #Run simulation
 echo "start running in directory $PWD"
@@ -48,7 +50,7 @@ echo ""
 
 #Move output files and cleanup
 echo "Cleaning Up..."
-mv -v eC.txt /eic/u/kdevereaux/work/multiplicity/condor/eC_18_110/outForPythiaMode/eC_${INPUT}.txt
-mv -v eC.root /eic/u/kdevereaux/work/multiplicity/condor/eC_18_110/outForPythiaMode/eC_${INPUT}.root
-mv -v eC.log /eic/u/kdevereaux/work/multiplicity/condor/eC_18_110/logs/eC_${INPUT}.log
+mv -v eC.txt ${WORKING_DIR}/outForPythiaMode/eC_${INPUT}.txt
+mv -v eC.root ${WORKING_DIR}/outForPythiaMode/eC_${INPUT}.root
+mv -v eC.log ${WORKING_DIR}/logs/eC_${INPUT}.log
 echo "DONE!!!"
