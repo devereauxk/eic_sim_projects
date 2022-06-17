a086eecc9dfe32badc373f52df0453cdbba8e418
Kyle Devereaux <devereauxk@berkeley.edu>
2022-06-17 13:53:32 -0400

    temp

--- reconstruction/run.sh
+++ reconstruction/run.sh
@@ -12,15 +12,15 @@ EA_DIR=./BeAGLE_v102/eXe_27.6_0_tauforOff_nlo
 #echo "generating hadron histograms for eA (writing to ana_merged.root file)..."
 #root -l -q "ana_hadron_gen.C(\"$EP_DIR/outForPythiaMode/merged.root\", \"$EP_DIR/outForPythiaMode/ana_merged.root\", 0, 0, 1)"
 
-echo "generating hadron histograms for eA (writing to ana_merged.root file)..."
-root -l -q "ana_hadron_gen.C(\"$EA_DIR/outForPythiaMode/merged.root\", \"$EA_DIR/outForPythiaMode/ana_merged.root\", 0, 1, 1)"
+#echo "generating hadron histograms for eA (writing to ana_merged.root file)..."
+#root -l -q "ana_hadron_gen.C(\"$EA_DIR/outForPythiaMode/merged.root\", \"$EA_DIR/outForPythiaMode/ana_merged.root\", 0, 1, 1)"
 
-#echo "generating histogram plots for eA/ep (writing to figs/ folder)..."
-#mkdir figs
-#mkdir $EA_DIR/figs_norm_0
-#root -l -q "plot_chadron_gen.C(\"$EP_DIR/outForPythiaMode/ana_merged.root\", 6, 5, \"$EA_DIR/outForPythiaMode/ana_merged.root\", 8, 5, \"$EA_DIR/figs_norm_0/hists_gen.root\", 0)"
-#mv figs/* $EA_DIR/figs_norm_0
-#rm -r figs
+echo "generating histogram plots for eA/ep (writing to figs/ folder)..."
+mkdir figs
+mkdir $EA_DIR/figs_norm_0
+root -l -q "plot_chadron_gen.C(\"$EP_DIR/outForPythiaMode/ana_merged.root\", 6, 5, \"$EA_DIR/outForPythiaMode/ana_merged.root\", 8, 5, \"$EA_DIR/figs_norm_0/hists_gen.root\", 0)"
+mv figs/* $EA_DIR/figs_norm_0
+rm -r figs
 
 #mkdir figs
 #mkdir $EA_DIR/figs_norm_1
