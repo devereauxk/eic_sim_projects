e12cae0850ed1936a5fdcd2587e2683c05bc3c72
Kyle Devereaux <devereauxk@berkeley.edu>
2022-07-09 15:52:05 -0700

    try

---
+++ reconstruction/inc_hadron_gen.C
@@ -0,0 +1,91 @@
+R__LOAD_LIBRARY(libeicsmear);
+
+#include "bins.h"
+
+const double degree = 180./TMath::Pi();
+
+const int verbosity = 1; // use this to control debugging messages
+
+using namespace std;
+
+void inc_hadron_gen(const char* inFile = "merged.root", const char* outFile = "inc_hadron.root", int nevt = 0, int data_type = 0, int gen_type = 0)
+{
+  cout << "Data Type: ";
+  if (data_type==0) cout << "EIC" << endl;
+  else if (data_type==1) cout << "HERMES" << endl;
+  else cout << "CLAS" << endl;
+
+  cout << "Generator Type: ";
+  if (gen_type==0) cout << "Pythia6" << endl;
+  else cout << "BeAGLE" << endl;
+
+  TFile *f = new TFile(inFile);
+
+  //Get EICTree Tree
+  TTree *tree = (TTree*)f->Get("EICTree");
+
+  Int_t nEntries = tree->GetEntries();
+  cout<<"-------------------------------"<<endl;
+  cout<<"Total Number of Events = "<<nEntries<<endl<<endl;
+
+  // Event Class note EventBeagle used instead of EventPythia
+  erhic::EventBeagle *event(NULL); //Note that I use Pointer
+
+  // Access event Branch
+  tree->SetBranchAddress("event",&event);
+
+  // Initialize histograms
+  TH3D* h3d_event_Ninc_vs_thickness_vs_b = new TH3D("h3d_event_Ninc_vs_thickness_vs_b","Ninc vs thickness vs b",100,0,10,100,0,10,100,0,10);
+  h3d_event_Ninc_vs_thickness_vs_b->Sumw2();
+  TH3D* h3d_event_Nincch_vs_thickness_vs_b = new TH3D("h3d_event_Nincch_vs_thickness_vs_b","Nincch vs thickness vs b",100,0,10,100,0,10,100,0,10);
+  h3d_event_Nincch_vs_thickness_vs_b->Sumw2();
+
+  //Loop Over Events
+  if (nevt == 0) nevt = nEntries;
+  for(Int_t ievt = 0; ievt < nevt; ievt++)
+  {
+    if (ievt%10000==0) cout<<"Processing event = "<<ievt<<"/"<<nevt<<endl;
+
+    tree->GetEntry(ievt);
+
+    //event level variables
+    double Q2 = event->GetTrueQ2();
+    double x = event->GetTrueX();
+    double nu = event->GetTrueNu();
+    double Ninc = event->NINC;
+    double Nincch = event->NINCch;
+    double thickness = event->Thickness;
+    double b = event->b;
+
+    //kinematic cuts
+    bool flag_event_select = true;
+    if (data_type==0)
+    { // EIC event selection (from Yuxiang)
+      if (event->GetTrueY()<0.05 || event->GetTrueY()>0.8) flag_event_select = false;
+    }
+    else if (data_type==1)
+    { // HERMES event selections (from HERMES paper)
+      if (event->GetTrueY()>0.85) flag_event_select = false;
+      if (event->GetTrueW2()<4) flag_event_select = false;
+      if (event->GetTrueNu()<6) flag_event_select = false;
+    }
+    else
+    { // CLAS event selection (to be implemented)
+
+    }
+    if (!flag_event_select) continue;
+
+    // fill histograms
+    h3d_event_Ninc_vs_thickness_vs_b->Fill(Ninc, thickness, b);
+    h3d_event_Nincch_vs_thickness_vs_b->Fill(Nincch, thickness, b);
+
+  } // end of event by event processing
+
+  //write histograms to outfile
+  TFile* fout = new TFile(outFile,"recreate");
+
+  h3d_event_Ninc_vs_thickness_vs_b->Write();
+  h3d_event_Nincch_vs_thickness_vs_b->Write();
+
+  fout->Write();
+}
---
+++ reconstruction/plot_inc_hadron.C
@@ -0,0 +1,64 @@
+#include "bins.h"
+#include "TStyle.h"
+#include "TGraphErrors.h"
+using namespace std;
+unsigned int verbosity = 0;
+
+const int sys_bins = 11;
+const char* sys_name[sys_bins] = {"e+p", "e+Au", "e+Cu", "e+Ca", "e+C", "e+p (BeAGLE)", "e+D", "e+Au (pythia)", "e+Xe", "e+C (pythia)", "e+Pb"};
+const char* sys_abbr[sys_bins] = {"ep", "eAu", "eCu", "eCa", "eC", "ep_BeAGLE", "eD", "eAu_pythia", "eXe", "eC_pythia", "ePb"};
+
+const int energy_bins = 6;
+const char* energy_name[energy_bins] = {"5x41 GeV", "10x100 GeV", "10x110 GeV", "18x110 GeV", "18x275 GeV", "27.6x0 GeV"};
+const char* energy_abbr[energy_bins] = {"5_41", "10_100", "10_110", "18_110", "18_275", "27.6_0"};
+
+static int cno = 0;
+
+const int MaxVarbin = 10;
+
+
+void plot_inc_hadron(const char* inFile = "inc_merged.root", const int sys_option = 0, const int energy_option = 0, const char* outDir = "figs/")
+{
+  mcs(-1);
+
+  TFile* fin = new TFile(inFile,"READ");
+
+  // load histograms
+  TH3D* h3d_event_Ninc_vs_thickness_vs_b = (TH3D*)fin->Get("h3d_event_Ninc_vs_thickness_vs_b");
+  TH3D* h3d_event_Nincch_vs_thickness_vs_b = (TH3D*)fin->Get("h3d_event_Nincch_vs_thickness_vs_b");
+
+  // 2d plots
+  TH2D* h2d_event_thickness_vs_b = (TH2D*)h3d_event_Ninc_vs_thickness_vs_b->Project3D("xy");
+  TH2D* h2d_event_Ninc_vs_thickness = (TH2D*)h3d_event_Ninc_vs_thickness_vs_b->Project3D("yx");
+  TH2D* h2d_event_Ninc_vs_b = (TH2D*)h3d_event_Ninc_vs_thickness_vs_b->Project3D("zx");
+
+  mcs(cno++)
+  {
+    float plot_xrange_lo = 0;
+    float plot_xrange_hi = 10;
+
+    float plot_yrange_lo = 0;
+    float plot_yrange_hi = 10;
+
+    TH2F* htemp = new TH2F("htemp","",10,plot_xrange_lo,plot_xrange_hi,10,plot_yrange_lo,plot_yrange_hi);
+    htemp->Draw();
+    htemp->GetXaxis()->SetTitle("thickness [fm]");
+    htemp->GetYaxis()->SetTitle("b [fm]");
+    myhset(htemp, 1.2, 1.6, 0.05, 0.045);
+
+    h2d_event_thickness_vs_b->Draw("samecolz");
+
+    h2d_hadron_z_vs_eta_gen_ep[iQ2][ivar]->Draw("samecolz");
+
+    TLatex* tl = new TLatex();
+    tl->SetTextAlign(11);
+    tl->SetTextSize(0.03);
+    tl->DrawLatexNDC(0.21,0.85,Form("%s @ %s",sys_name[sys_ep_option],energy_name[energy_ep_option]));
+
+    gROOT->ProcessLine( Form("cc%d->Print(\"%sthickness_vs_b.pdf\")", cno-1, outDir) );
+
+    delete htemp;
+    delete tl;
+  }
+
+}
