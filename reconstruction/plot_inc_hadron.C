612c5b4cd936e4468e37599629650aeff6f6e25f
Kyle Devereaux <devereauxk@berkeley.edu>
2022-07-09 20:29:23 -0700

    try

--- reconstruction/plot_inc_hadron.C
+++ reconstruction/plot_inc_hadron.C
@@ -41,9 +41,9 @@ void plot_inc_hadron(const char* inFile = "inc_merged.root", const int sys_optio
   TH1D* h1d_event_Nincch = (TH1D*)h3d_event_Nincch_vs_thickness_vs_b->ProjectionX("Nincc");
 
   // 2d plots
-  TH2D* h2d_event_thickness_vs_b = (TH2D*)h3d_event_Ninc_vs_thickness_vs_b->Project3D("yz");
-  TH2D* h2d_event_Ninc_vs_thickness = (TH2D*)h3d_event_Ninc_vs_thickness_vs_b->Project3D("yx");
-  TH2D* h2d_event_Ninc_vs_b = (TH2D*)h3d_event_Ninc_vs_thickness_vs_b->Project3D("zx");
+  TH2D* h2d_event_thickness_vs_b = (TH2D*)h3d_event_Ninc_vs_thickness_vs_b->Project3D("zy");
+  TH2D* h2d_event_Ninc_vs_thickness = (TH2D*)h3d_event_Ninc_vs_thickness_vs_b->Project3D("xy");
+  TH2D* h2d_event_Ninc_vs_b = (TH2D*)h3d_event_Ninc_vs_thickness_vs_b->Project3D("xz");
 
   // profiles of 2d plots
   TProfile* prof_thickness_in_b = (TProfile*)h2d_event_thickness_vs_b->ProfileX("prof_thickness_in_b");
@@ -171,7 +171,7 @@ void plot_inc_hadron(const char* inFile = "inc_merged.root", const int sys_optio
     htemp->GetYaxis()->SetTitle("b [fm]");
     myhset(htemp, 1.2, 1.6, 0.05, 0.045);
 
-    h2d_event_thickness_vs_b->Draw("colz");
+    h2d_event_thickness_vs_b->Draw("colzsame");
 
     TLatex* tl = new TLatex();
     tl->SetTextAlign(11);
