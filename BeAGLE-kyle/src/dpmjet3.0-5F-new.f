d54d2a3c9f650f3933b77a561e7bec2849b4fd05
Kyle Devereaux <devereauxk@berkeley.edu>
2022-07-24 17:02:34 -0700

    print statements not working

--- BeAGLE-kyle/src/dpmjet3.0-5F-new.f
+++ BeAGLE-kyle/src/dpmjet3.0-5F-new.f
@@ -9565,8 +9565,7 @@ C9990 CONTINUE
 
       IREJ = 0
 
-  999 FORMAT(1X, "@debug : DT_INUCAS : called on IDXCAS = ", I5, " which has NCAS = ", I3, ", IDCH = ", I3, ", NOBAM = ", I3)
-      WRITE(LOUT,999) IDXCAS, NCAS, IDCH(IDXCAS), NOBAM(IDXCAS)
+      WRITE(LOUT, '(a,I5,a,I3,a,I3,a,I3)') "@debug : DT_INUCAS : called on IDXCAS = ", IDXCAS, " which has NCAS = ", NCAS, ", IDCH = ", IDCH(IDXCAS), ", NOBAM = ", NOBAM(IDXCAS)
 
 * update counter
       IF (NINCEV(1).NE.NEVHKK) THEN
@@ -9973,8 +9972,7 @@ C              ENDIF
 * sample secondary interaction
             IDNUC = IDBAM(IDXSPE(1))
 
- 1002       FORMAT(1X, "@kdebug : DT_HADRIN : called from DT_INUCAS on particle IDXCAS = ", I5, ", IDPR = ", I5, ", IDTA = ", I5, ", MODE = ", I5)
-            WRITE(LOUT,1002) IDXCAS, IDCAS, IDNUC, IPROC
+            WRITE(LOUT,'(a,I5,a,I5,a,I5,a,I3)') "@kdebug : DT_HADRIN : called from DT_INUCAS on particle IDXCAS = ", IDXCAS, ", IDPR = ", IDCAS, ", IDTA = ", IDNUC, ", MODE = ", IPROC
 
             CALL DT_HADRIN(IDCAS,PCAS1,IDNUC,PNUC,IPROC,IREJ1)
             IF (IREJ1.EQ.1) GOTO 9999
