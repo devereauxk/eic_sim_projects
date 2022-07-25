6383caee311408e1f59f5fbd54d5fcb4ac8f7629
Kyle Devereaux <devereauxk@berkeley.edu>
2022-07-25 14:16:57 -0700

    debug lines, read_kdebug.txt moded

--- BeAGLE-debug/read_kdebug.sh
+++ BeAGLE-debug/read_kdebug.sh
@@ -1,11 +1,37 @@
-fin='eAu_10_100_qhat0_nlo/logs/eAu_0.log'
+fin_dir=eAu_10_100_qhat0_nlo/logs
+fout=kdebug_bin.txt
+#fin_dir=test_dir
+#fout=test_dir/kdebug_bin_test.txt
+rm $fout
 
-echo "reading through $fin for @kdebug statements ... "
+n_files=`ls $fin_dir/*.log | wc -l`
+echo "reading through all $n_files $fin_dir/*.log files for @kdebug statements ... "
 
-while read line; do
-  first_word=`echo $line | awk '{print $1;}'`
-  if [[ $first_word == "@kdebug" ]]
-  then
-    echo $line
-  fi
-done < $fin
+n=1
+for fin in $fin_dir/*.log; do
+
+  #n_lines=`wc -l $fin | awk '{ print $1 }'`
+  #n=0
+
+  while read line; do
+    # if line of file starts with "@kdebug " then prints whole line to $fout
+    first_word=`echo $line | awk '{print $1;}'`
+    if [[ $first_word == "@kdebug" ]]
+    then
+      echo $line >> $fout
+    fi
+
+    # counts through lines of file and prints progress to screen
+    #if [[ $(($n%10)) == 0 ]]
+    #then
+    #  echo "$n/$n_lines"
+    #fi
+    #n=$((n+1))
+
+  done < $fin
+
+  # counts through number of files read and print progress to screen
+  echo "$n/$n_files"
+  n=$((n+1))
+
+done
