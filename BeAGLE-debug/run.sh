f8269bdd75ccf4c9e788124ce5282a1504b6eaaa
Kyle Devereaux <devereauxk@berkeley.edu>
2022-07-30 23:08:47 -0400

    debug lines eAu and eC with double ration plots

--- BeAGLE-debug/run.sh
+++ BeAGLE-debug/run.sh
@@ -1,5 +1,5 @@
 EP_DIR=../reconstruction/BeAGLE_v101/ep_10_100_baseline_parp2
-EA_DIR=./eAu_10_100_qhat0_nlo
+EA_DIR=./eC_10_100_qhat0_nlo
 
 
 echo "merging root eA root files..."
@@ -11,13 +11,13 @@ root -l -q "ana_hadron_gen.C(\"$EA_DIR/outForPythiaMode/merged.root\", \"$EA_DIR
 echo "generating histogram plots for eA/ep (writing to figs/ folder)..."
 mkdir figs
 mkdir $EA_DIR/figs_norm_0
-root -l -q "plot_chadron_gen.C(\"$EP_DIR/outfiles/ana_merged.root\", 0, 1, \"$EA_DIR/outForPythiaMode/ana_merged.root\", 1, 1, \"$EA_DIR/figs_norm_0/hists_gen.root\", 0)"
+root -l -q "plot_chadron_gen.C(\"$EP_DIR/outfiles/ana_merged.root\", 0, 1, \"$EA_DIR/outForPythiaMode/ana_merged.root\", 4, 1, \"$EA_DIR/figs_norm_0/hists_gen.root\", 0)"
 mv figs/* $EA_DIR/figs_norm_0
 rm -r figs
 
 mkdir figs
 mkdir $EA_DIR/figs_norm_1
-root -l -q "plot_chadron_gen.C(\"$EP_DIR/outfiles/ana_merged.root\", 0, 1, \"$EA_DIR/outForPythiaMode/ana_merged.root\", 1, 1, \"$EA_DIR/figs_norm_1/hists_gen.root\", 1)"
+root -l -q "plot_chadron_gen.C(\"$EP_DIR/outfiles/ana_merged.root\", 0, 1, \"$EA_DIR/outForPythiaMode/ana_merged.root\", 4, 1, \"$EA_DIR/figs_norm_1/hists_gen.root\", 1)"
 mv figs/* $EA_DIR/figs_norm_1
 rm -r figs
 
