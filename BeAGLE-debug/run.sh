30574d89acdf29b45b2566c91b9a6ce435b1907c
Kyle Devereaux <devereauxk@berkeley.edu>
2022-07-30 19:56:29 -0700

    double ratio files added to BeAGLE-debug

---
+++ BeAGLE-debug/run.sh
@@ -0,0 +1,31 @@
+EP_DIR=../reconstruction/BeAGLE_v101/ep_10_100_baseline_parp2
+EA_DIR=./eAu_10_100_qhat0_nlo
+
+
+echo "merging root eA root files..."
+hadd -f -j $EA_DIR/outForPythiaMode/merged.root $EA_DIR/outForPythiaMode/*.root
+
+echo "generating hadron histograms for eA (writing to ana_merged.root file)..."
+root -l -q "ana_hadron_gen.C(\"$EA_DIR/outForPythiaMode/merged.root\", \"$EA_DIR/outForPythiaMode/ana_merged.root\", 0, 0, 1)"
+
+echo "generating histogram plots for eA/ep (writing to figs/ folder)..."
+mkdir figs
+mkdir $EA_DIR/figs_norm_0
+root -l -q "plot_chadron_gen.C(\"$EP_DIR/outfiles/ana_merged.root\", 0, 1, \"$EA_DIR/outForPythiaMode/ana_merged.root\", 1, 1, \"$EA_DIR/figs_norm_0/hists_gen.root\", 0)"
+mv figs/* $EA_DIR/figs_norm_0
+rm -r figs
+
+mkdir figs
+mkdir $EA_DIR/figs_norm_1
+root -l -q "plot_chadron_gen.C(\"$EP_DIR/outfiles/ana_merged.root\", 0, 1, \"$EA_DIR/outForPythiaMode/ana_merged.root\", 1, 1, \"$EA_DIR/figs_norm_1/hists_gen.root\", 1)"
+mv figs/* $EA_DIR/figs_norm_1
+rm -r figs
+
+
+#const int sys_bins = 11;
+#const char* sys_name[sys_bins] = {"e+p", "e+Au", "e+Cu", "e+Ca", "e+C", "e+p (BeAGLE)", "e+D", "e+Au (pythia)", "e+Xe", "e+C (pythia)", "e+Pb"};
+#const char* sys_abbr[sys_bins] = {"ep", "eAu", "eCu", "eCa", "eC", "ep_BeAGLE", "eD", "eAu_pythia", "eXe", "eC_pythia", "ePb"};
+#
+#const int energy_bins = 6;
+#const char* energy_name[energy_bins] = {"5x41 GeV", "10x100 GeV", "10x110 GeV", "18x110 GeV", "18x275 GeV", "27.6x0 GeV"};
+#const char* energy_abbr[energy_bins] = {"5_41", "10_100", "10_110", "18_110", "18_275", "27.6_0"};
---
+++ BeAGLE-debug/run_kdebug_eAu.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/bash
+#-----#-----#----#----#----#----#----#----#----#
+
+WORKING_DIR=/eic/u/kdevereaux/work/BeAGLE-debug/eC_10_100_qhat0_nlo
+
+if [ -z "$1" ]
+then
+        echo "No job number set."
+        echo "Please run as ./run_kdebug.sh jobnumber"
+        echo "Exiting..."
+        exit 1
+fi
+
+#Go into scratch directory
+chmod g+rx ${_CONDOR_SCRATCH_DIR}
+cd ${_CONDOR_SCRATCH_DIR}
+
+#Make subdirectory and move there
+INPUT=$(( 0 + $1 ))
+echo $INPUT
+DIR=`printf "%04d" $INPUT`
+mkdir $DIR
+cd $DIR
+
+#Environmental Variables
+source /cvmfs/sphenix.opensciencegrid.org/gcc-8.3/opt/sphenix/core/gcc/8.3.0.1-0a5ad/x86_64-centos7/setup.sh
+
+#Soft links to necessary files
+rm ${WORKING_DIR}/logs/kdebug_bin_${INPUT}.txt
+touch ${WORKING_DIR}/logs/kdebug_bin_${INPUT}.txt
+ln -s ${WORKING_DIR}/logs/eC_${INPUT}.log
+
+
+echo "start running in directory $PWD"
+
+echo "Running Job Number $1"
+while read line; do
+  # if line of file starts with "@kdebug " then prints whole line to $fout
+  first_word=`echo $line | awk '{print $1;}'`
+  if [[ $first_word == "@kdebug" ]]
+  then
+    echo $line >> ${WORKING_DIR}/logs/kdebug_bin_${INPUT}.txt
+  fi
+done < eC_${INPUT}.log
+echo "Completed!!!"
+
+echo ""
