55f20fa896e5b11a1db17522e0e823e492157801
Kyle Devereaux <devereauxk@berkeley.edu>
2022-07-30 23:36:45 -0400

    issue with kdebugs

--- BeAGLE-debug/combine_kdebug.sh
+++ BeAGLE-debug/combine_kdebug.sh
@@ -1,5 +1,5 @@
-fin_dir=/eic/u/kdevereaux/work/BeAGLE-debug/eC_10_100_qhat0_nlo/logs
-fout=/eic/u/kdevereaux/work/BeAGLE-debug/eC_10_100_qhat0_nlo/kdebug_bin.txt
+fin_dir=/eic/u/kdevereaux/work/BeAGLE-debug/eAu_10_100_qhat0_nlo/logs
+fout=/eic/u/kdevereaux/work/BeAGLE-debug/eAu_10_100_qhat0_nlo/kdebug_bin.txt
 rm $fout
 
 for fin in $fin_dir/kdebug_bin*; do
