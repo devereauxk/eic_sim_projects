3738ea50cfa4b73d03c500de32ca1c859d83ed65
Kyle Devereaux <devereauxk@berkeley.edu>
2022-07-30 20:01:57 -0700

    kdebug_combine modified to support multiple runs

--- BeAGLE-debug/combine_kdebug.sh
+++ BeAGLE-debug/combine_kdebug.sh
@@ -1,5 +1,5 @@
 fin_dir=/eic/u/kdevereaux/work/BeAGLE-debug/eAu_10_100_qhat0_nlo/logs
-fout=kdebug_bin.txt
+fout=/eic/u/kdevereaux/work/BeAGLE-debug/eAu_10_100_qhat0_nlo/kdebug_bin.txt
 rm $fout
 
 for fin in $fin_dir/kdebug_bin*; do
