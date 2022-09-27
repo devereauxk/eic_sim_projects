cd141f7b62c3d86a5339aefe8fd0be24640022e0
Kyle Devereaux <devereauxk@berkeley.edu>
2022-09-27 16:10:24 -0700

    energy-energy correlator

--- energy_correlator/plot_eec_hists.C
+++ energy_correlator/plot_eec_hists.C
@@ -31,7 +31,7 @@ void individual_hists(const char* out_dir)
   }
 
   // 1d jet eec histogram, log bins
-  mclogx(cno++);
+  mcs(cno++);
   {
     float plot_xrange_lo = 1E-4;
     float plot_xrange_hi = 1;
@@ -39,13 +39,15 @@ void individual_hists(const char* out_dir)
     float plot_yrange_lo = 0;
     float plot_yrange_hi = 70;
 
+    /*
     TH2F htemp("htemp","",10,plot_xrange_lo,plot_xrange_hi,10,plot_yrange_lo,plot_yrange_hi);
     htemp.Draw("hsame");
     htemp.GetXaxis()->SetTitle("R_{L}");
     htemp.GetYaxis()->SetTitle("normalized EEC");
     myhset(&htemp,1.2,1.6,0.05,0.05);
+    */
 
-    h1d_jet_pt->Draw("same");
+    h1d_jet_pt->Draw();
 
     gROOT->ProcessLine( Form("cc%d->Print(\"%sh1d_jet_eec.pdf\")", cno-1, out_dir) );
   }
