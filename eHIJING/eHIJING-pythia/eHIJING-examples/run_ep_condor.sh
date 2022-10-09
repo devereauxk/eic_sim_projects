9edc5afd8c55cda3f077c622f9febfdf7aeae987
Kyle Devereaux <devereauxk@berkeley.edu>
2022-10-08 21:47:48 -0700

    eec_hists, ehijing-test modded to support eec calculation

--- eHIJING/eHIJING-pythia/eHIJING-examples/run_ep_condor.sh
+++ eHIJING/eHIJING-pythia/eHIJING-examples/run_ep_condor.sh
@@ -24,7 +24,7 @@ export PYTHIA8=/eic/u/kdevereaux/work/eHIJING/local
 
 exe=/eic/u/kdevereaux/work/eHIJING/eHIJING-pythia/eHIJING-examples/build/ehijing-test
 Configfile=/eic/u/kdevereaux/work/eHIJING/eHIJING-pythia/eHIJING-examples/s20.setting
-Neve=100 #10000
+Neve=10000 #10000
 
 K=2.0
 M=1 # Generlizaed HT:1,  HIgher-twist:0, both in the soft gluon emission limit.
