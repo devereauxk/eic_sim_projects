b342345a1285aba78b15dff2c8d418316840e444
Kyle Devereaux <devereauxk@berkeley.edu>
2022-10-08 21:44:27 -0700

    eec_hists, ehijing-test modded to support eec calculation

--- eHIJING/eHIJING-pythia/eHIJING-examples/run_ep_condor.sh
+++ eHIJING/eHIJING-pythia/eHIJING-examples/run_ep_condor.sh
@@ -38,12 +38,6 @@ Z=1
 A=1
 $exe $Neve $Z $A $M $K $DIR $DIR $Configfile # > /dev/null 2>&1
 
-echo `pwd`
-echo `ls ../`
-echo `ls`
-echo "====="
-echo `ls $DIR`
-
 mv -v $DIR/events.dat $folder/ep_${INPUT}.dat
 mv -v $DIR/GHT.dat $TablePath/GHT_${INPUT}.dat
 mv -v $DIR/Qs.dat $TablePath/Qs_${INPUT}.dat
