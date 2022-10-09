5b9bf49e8e62a1c1c4a88f26047f9d51eca10d73
Kyle Devereaux <devereauxk@berkeley.edu>
2022-10-08 21:12:26 -0700

    eec_hists, ehijing-test modded to support eec calculation

--- eHIJING/eHIJING-pythia/eHIJING-examples/run_ep_condor.sh
+++ eHIJING/eHIJING-pythia/eHIJING-examples/run_ep_condor.sh
@@ -25,18 +25,20 @@ export PYTHIA8=/eic/u/kdevereaux/work/eHIJING/local
 
 exe=/eic/u/kdevereaux/work/eHIJING/eHIJING-pythia/eHIJING-examples/build/ehijing-test
 Configfile=/eic/u/kdevereaux/work/eHIJING/eHIJING-pythia/eHIJING-examples/s20.setting
-Neve=10000 #10000
+Neve=100 #10000
 
 K=2.0
 M=1 # Generlizaed HT:1,  HIgher-twist:0, both in the soft gluon emission limit.
 
 label="ep_1E6_condor"
 folder=$OUT_DIR/Events/$label
-TablePath=$OUT_DIR/Tables/$K
-local_folder=./Events
-local_TablePath=./Tables
+TablePath=$OUT_DIR/Tables/$label
 mkdir -p $folder
 mkdir -p $TablePath
 Z=1
 A=1
-$exe $Neve $Z $A $M $K $TablePath $folder $Configfile # > /dev/null 2>&1
+$exe $Neve $Z $A $M $K $DIR $DIR $Configfile # > /dev/null 2>&1
+
+mv -v events.dat $folder/ep_${INPUT}.dat
+mv -v GHT.dat $TablePath/GHT_${INPUT}.dat
+mv -v Qs.dat $TablePath/Qs_${INPUT}.dat
