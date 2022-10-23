8ed095acf5d928edbdcd1ec119fa8fbca8623611
Kyle Devereaux <devereauxk@berkeley.edu>
2022-10-23 12:00:20 -0700

    res plots

--- resolution/convert_res_csv_to_root.C
+++ resolution/convert_res_csv_to_root.C
@@ -70,7 +70,7 @@ void convert_res_csv_to_root(const char* inFile = "for_Wenqing.csv", const char*
   // secondary plots written to current directory
   mcs(-1);
 
-  tree->Draw("Eta:Momentum:DeltaP>>temp");
+  tree->Draw("DeltaP:Eta:Momentum>>temp");
   TH3F* temp = (TH3F*)gDirectory->Get("temp");
   TH2F* mom_deltap = (TH2F*)temp->Project3D("xy");
 
