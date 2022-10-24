40ff222bb4aeb5f665e878e0b6a27569f7df1494
Kyle Devereaux <devereauxk@berkeley.edu>
2022-10-23 18:54:39 -0700

    res plots

--- resolution/convert_res_csv_to_root.C
+++ resolution/convert_res_csv_to_root.C
@@ -70,7 +70,7 @@ void convert_res_csv_to_root(const char* inFile = "for_Wenqing.csv", const char*
   // secondary plots written to current directory
   mcs(-1);
 
-  tree->Draw("DeltaP:Eta:Momentum>>temp(19,0,120,19,-3.5,3.5,1000,0,1)", "Eta != 0.9 && Eta != -0.9");
+  tree->Draw("DeltaP:Eta:Momentum>>temp(15,0,120,19,-3.5,3.5,1000,0,1)", "Eta != 0.9 && Eta != -0.9");
   TH3F* temp = (TH3F*)gDirectory->Get("temp");
   TProfile2D* mom_deltap = (TProfile2D*)temp->Project3DProfile("xy");
 
@@ -82,6 +82,33 @@ void convert_res_csv_to_root(const char* inFile = "for_Wenqing.csv", const char*
     mom_deltap->Draw("colz");
     gROOT->ProcessLine( Form("cc%d->Print(\"%sDeltaP_vs_eta_momentum.pdf\")", cno-1, "./") );
   }
+
+  tree->Draw("DCArPhi:Eta:Momentum>>temp1(15,0,120,19,-3.5,3.5,1000,0,100)", "Eta != 0.9 && Eta != -0.9");
+  TH3F* temp1 = (TH3F*)gDirectory->Get("temp1");
+  TProfile2D* mom_deltap1 = (TProfile2D*)temp1->Project3DProfile("xy");
+
+  mcs(cno++, 0, 0, 800, 800, 0.1, 0.27, 0.1, 0.23);
+  {
+    mom_deltap1->GetYaxis()->SetRangeUser(0,120);
+    mom_deltap1->GetXaxis()->SetTitle("#eta");
+    mom_deltap1->GetYaxis()->SetTitle("p [GeV]");
+    mom_deltap1->Draw("colz");
+    gROOT->ProcessLine( Form("cc%d->Print(\"%sDCArPhi_vs_eta_momentum.pdf\")", cno-1, "./") );
+  }
+
+  tree->Draw("DCAz:Eta:Momentum>>temp2(15,0,120,19,-3.5,3.5,1000,0,1)", "Eta != 0.9 && Eta != -0.9");
+  TH3F* temp2 = (TH3F*)gDirectory->Get("temp2");
+  TProfile2D* mom_deltap2 = (TProfile2D*)temp2->Project3DProfile("xy");
+
+  mcs(cno++, 0, 0, 800, 800, 0.1, 0.27, 0.1, 0.23);
+  {
+    mom_deltap2->GetYaxis()->SetRangeUser(0,120);
+    mom_deltap2->GetXaxis()->SetTitle("#eta");
+    mom_deltap2->GetYaxis()->SetTitle("p [GeV]");
+    mom_deltap2->Draw("colz");
+    gROOT->ProcessLine( Form("cc%d->Print(\"%sDCAz_vs_eta_momentum.pdf\")", cno-1, "./") );
+  }
+
 
 
 
