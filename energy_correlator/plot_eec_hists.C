b7d8255f250fcfba24287d35a8b24a9a2255186b
Kyle Devereaux <devereauxk@berkeley.edu>
2022-11-08 13:07:21 -0800

    new normalizations and ratio definitions; EEC

--- energy_correlator/plot_eec_hists.C
+++ energy_correlator/plot_eec_hists.C
@@ -13,12 +13,15 @@ static double eta_lo[etabin] = {-3.5, -1, 1, -3.5};
 static double eta_hi[etabin] = {-1, 1, 3.5, 3.5};
 const int eta_color[etabin] = {kGreen+1, kBlue, kViolet, kOrange+1};
 
-TH1D* h1d_jet_eec[etabin][ptbin] = {};
-TH1D* h1d_jet_eec_rlsqrtpt[ptbin] = {};
 TH1D* h1d_jet_pt = NULL;
 TH1D* h1d_jet_eta = NULL;
 
+TH1D* h1d_jet_eec[etabin][ptbin] = {};
+TH1D* h1d_jet_eec_norm[etabin][ptbin] = {};
 TH1D* h1d_jet_eec_baseline[etabin][ptbin] = {};
+TH1D* h1d_jet_eec_baseline_norm[etabin][ptbin] = {};
+
+TH1D* h1d_jet_eec_rlsqrtpt[ptbin] = {};
 TH1D* h1d_jet_eec_rlsqrtpt_baseline[ptbin] = {};
 
 static int cno = 0;
@@ -60,46 +63,17 @@ void individual_hists(const char* out_dir)
     gROOT->ProcessLine( Form("cc%d->Print(\"%sh1d_jet_eta.pdf\")", cno-1, out_dir) );
   }
 
-  // 1d jet eec histogram, log bins
-
-  /*
-  TCanvas* c = new TCanvas("c","c", 800, 800);
-  c->SetLogx();
-  c->SetLogy();
-  c->Range(0,0,1,1);
-  c->SetLeftMargin(0.15);
-  c->SetRightMargin(0.15);
-  c->SetBottomMargin(0.1);
-
-  h1d_jet_eec->Draw("hist e");
-
-  c->SaveAs( Form("%sh1d_jet_eec.pdf", out_dir));
-  */
   for (int ieta = 0; ieta < etabin; ieta++)
   {
     for (int ipt = 0; ipt < ptbin; ipt++)
     {
       mclogxy(cno++);
       {
-        /*
-        float plot_xrange_lo = 0;
-        float plot_xrange_hi = 1;
-
-        float plot_yrange_lo = 0;
-        float plot_yrange_hi = h1d_jet_eec[ipt]->GetMaximum()*1.50;
-
-        TH2F htemp("htemp","",50,plot_xrange_lo,plot_xrange_hi,50,plot_yrange_lo,plot_yrange_hi);
-        htemp.Draw("hsame");
-        htemp.GetXaxis()->SetTitle("R_{L}");
-        htemp.GetYaxis()->SetTitle("normalized EEC");
-        myhset(&htemp,1.2,1.6,0.05,0.05);
-        */
-
-        h1d_jet_eec[ieta][ipt]->GetXaxis()->SetTitle("R_{L}");
-        h1d_jet_eec[ieta][ipt]->GetYaxis()->SetTitle("normalized EEC");
-        h1d_jet_eec[ieta][ipt]->GetXaxis()->SetTitleOffset(1.3);
-        h1d_jet_eec[ieta][ipt]->GetYaxis()->SetTitleOffset(1.5);
-        h1d_jet_eec[ieta][ipt]->Draw("same hist e");
+        h1d_jet_eec_norm[ieta][ipt]->GetXaxis()->SetTitle("R_{L}");
+        h1d_jet_eec_norm[ieta][ipt]->GetYaxis()->SetTitle("normalized EEC");
+        h1d_jet_eec_norm[ieta][ipt]->GetXaxis()->SetTitleOffset(1.3);
+        h1d_jet_eec_norm[ieta][ipt]->GetYaxis()->SetTitleOffset(1.5);
+        h1d_jet_eec_norm[ieta][ipt]->Draw("same hist e");
 
         gROOT->ProcessLine( Form("cc%d->Print(\"%sh1d_jet_eec_%d_%d.pdf\")", cno-1, out_dir, ieta, ipt) );
       }
@@ -128,14 +102,14 @@ void overlay_hists(const char* out_dir)
 
       for (int ipt = 0; ipt < ptbin-1; ipt++)
       {
-        h1d_jet_eec[ieta][ipt]->GetXaxis()->SetRangeUser(plot_xrange_lo,plot_xrange_hi);
-        h1d_jet_eec[ieta][ipt]->GetYaxis()->SetRangeUser(plot_yrange_lo,plot_yrange_hi);
-        h1d_jet_eec[ieta][ipt]->SetMarkerColor(pt_color[ipt]);
-        h1d_jet_eec[ieta][ipt]->SetLineColor(pt_color[ipt]);
-        h1d_jet_eec[ieta][ipt]->SetMarkerSize(0.5);
-        h1d_jet_eec[ieta][ipt]->SetMarkerStyle(21);
-        h1d_jet_eec[ieta][ipt]->Draw("same hist e");
-        leg->AddEntry(h1d_jet_eec[ieta][ipt],Form("%.1f GeV < p_{T} < %.1f GeV",pt_lo[ipt],pt_hi[ipt]));
+        h1d_jet_eec_norm[ieta][ipt]->GetXaxis()->SetRangeUser(plot_xrange_lo,plot_xrange_hi);
+        h1d_jet_eec_norm[ieta][ipt]->GetYaxis()->SetRangeUser(plot_yrange_lo,plot_yrange_hi);
+        h1d_jet_eec_norm[ieta][ipt]->SetMarkerColor(pt_color[ipt]);
+        h1d_jet_eec_norm[ieta][ipt]->SetLineColor(pt_color[ipt]);
+        h1d_jet_eec_norm[ieta][ipt]->SetMarkerSize(0.5);
+        h1d_jet_eec_norm[ieta][ipt]->SetMarkerStyle(21);
+        h1d_jet_eec_norm[ieta][ipt]->Draw("same hist e");
+        leg->AddEntry(h1d_jet_eec_norm[ieta][ipt],Form("%.1f GeV < p_{T} < %.1f GeV",pt_lo[ipt],pt_hi[ipt]));
       }
       leg->Draw("same");
 
@@ -166,7 +140,7 @@ void overlay_hists(const char* out_dir)
       h1d_jet_eec_rlsqrtpt[ipt]->SetMarkerStyle(21);
       h1d_jet_eec_rlsqrtpt[ipt]->Draw("hist same");
       h1d_jet_eec_rlsqrtpt[ipt]->GetXaxis()->SetTitle("R_{L} #sqrt{p_{T,jet}}");
-      h1d_jet_eec_rlsqrtpt[ipt]->GetYaxis()->SetTitle("normalized EEC");
+      h1d_jet_eec_rlsqrtpt[ipt]->GetYaxis()->SetTitle("EEC (no normalization)");
       leg->AddEntry(h1d_jet_eec_rlsqrtpt[ipt],Form("%.1f GeV < p_{T} < %.1f GeV",pt_lo[ipt],pt_hi[ipt]));
     }
     leg->Draw("same");
@@ -196,8 +170,8 @@ void ratio_hists(const char* out_dir)
       for (int ipt = 0; ipt < ptbin-2; ipt++)
       {
         // calculate ratio
-        TH1D* ratio = (TH1D*) h1d_jet_eec[ieta][ipt]->Clone("ratio");
-        ratio->Divide(h1d_jet_eec_baseline[ieta][ipt]);
+        TH1D* ratio = (TH1D*) h1d_jet_eec_norm[ieta][ipt]->Clone("ratio");
+        ratio->Divide(h1d_jet_eec_baseline_norm[ieta][ipt]);
 
         // plot
         ratio->GetXaxis()->SetRangeUser(plot_xrange_lo,plot_xrange_hi);
@@ -230,10 +204,12 @@ void ratio_hists(const char* out_dir)
   // ratio hists for h1d_jet_eec_rlsqrtpt
   mclogxy(cno++);
   {
+    /*
     float plot_xrange_lo = 1E-1;
     float plot_xrange_hi = 5;
     float plot_yrange_lo = 0.5;
     float plot_yrange_hi = 5;
+    */
 
     TLegend* leg = new TLegend(0.21,0.7,0.51,0.82);
     leg->SetBorderSize(0);
@@ -245,13 +221,14 @@ void ratio_hists(const char* out_dir)
     {
       // calculate ratio
       TH1D* ratio = (TH1D*) h1d_jet_eec_rlsqrtpt[ipt]->Clone("ratio");
+      ratio->Add(h1d_jet_eec_rlsqrtpt_baseline, -1);
       ratio->Divide(h1d_jet_eec_rlsqrtpt_baseline[ipt]);
 
       // plot
-      ratio->GetXaxis()->SetRangeUser(plot_xrange_lo,plot_xrange_hi);
-      ratio->GetYaxis()->SetRangeUser(plot_yrange_lo,plot_yrange_hi);
+      //ratio->GetXaxis()->SetRangeUser(plot_xrange_lo,plot_xrange_hi);
+      //ratio->GetYaxis()->SetRangeUser(plot_yrange_lo,plot_yrange_hi);
       ratio->GetXaxis()->SetTitle("R_{L}#sqrt{p_{T,jet}}");
-      ratio->GetYaxis()->SetTitle("normalized EEC; energy loss on / off");
+      ratio->GetYaxis()->SetTitle("EEC; (energy loss on - off) / off");
       ratio->SetMarkerColor(pt_color[ipt]);
       ratio->SetLineColor(pt_color[ipt]);
       ratio->SetMarkerSize(0.5);
@@ -289,17 +266,21 @@ void plot_eec_hists(const char* fin_name = "hists_eec.root", const char* out_dir
   {
     for (int ipt = 0; ipt < ptbin; ipt++)
     {
+      // raw data histogram
       h1d_jet_eec[ieta][ipt] = (TH1D*) fin->Get(Form("h1d_jet_eec_%d_%d", ieta, ipt));
       h1d_jet_eec[ieta][ipt]->SetName(Form("h1d_jet_eec_%d_%d", ieta, ipt));
-      h1d_jet_eec[ieta][ipt]->Scale(1/h1d_jet_eec[ieta][ipt]->Integral()); // normalization
+
+      // normalized histogram
+      h1d_jet_eec_norm[ieta][ipt] = (TH1D*) h1d_jet_eec[ieta][ipt]->Clone(Form("h1d_jet_eec_%d_%d_norm", ieta, ipt));
+      h1d_jet_eec_norm[ieta][ipt]->Scale(1/h1d_jet_eec_norm[ieta][ipt]->Integral()); // normalization
     }
   }
 
   for (int ipt = 0; ipt < ptbin; ipt++)
   {
+    // raw data histogram
     h1d_jet_eec_rlsqrtpt[ipt] = (TH1D*) fin->Get(Form("h1d_jet_eec_rlsqrtpt_%d", ipt));
     h1d_jet_eec_rlsqrtpt[ipt]->SetName(Form("h1d_jet_eec_rlsqrtpt_%d", ipt));
-    h1d_jet_eec_rlsqrtpt[ipt]->Scale(1/h1d_jet_eec_rlsqrtpt[ipt]->Integral()); // normalization
   }
 
 
@@ -318,17 +299,20 @@ void plot_eec_hists(const char* fin_name = "hists_eec.root", const char* out_dir
     {
       for (int ipt = 0; ipt < ptbin; ipt++)
       {
+        // raw data histogram
         h1d_jet_eec_baseline[ieta][ipt] = (TH1D*) fin_baseline->Get(Form("h1d_jet_eec_%d_%d", ieta, ipt));
-        h1d_jet_eec_baseline[ieta][ipt]->SetName(Form("h1d_jet_eec_%d_%d", ieta, ipt));
-        h1d_jet_eec_baseline[ieta][ipt]->Scale(1/h1d_jet_eec_baseline[ieta][ipt]->Integral()); // normalization
+        h1d_jet_eec_baseline[ieta][ipt]->SetName(Form("h1d_jet_eec_%d_%d_baseline", ieta, ipt));
+
+        // normalized histogram
+        h1d_jet_eec_baseline_norm[ieta][ipt] = (TH1D*) h1d_jet_eec_baseline[ieta][ipt]->Clone(Form("h1d_jet_eec_%d_baseline_norm", ieta, ipt));
+        h1d_jet_eec_baseline_norm[ieta][ipt]->Scale(1/h1d_jet_eec_baseline_norm[ieta][ipt]->Integral()); // normalization
       }
     }
 
     for (int ipt = 0; ipt < ptbin; ipt++)
     {
       h1d_jet_eec_rlsqrtpt_baseline[ipt] = (TH1D*) fin_baseline->Get(Form("h1d_jet_eec_rlsqrtpt_%d", ipt));
-      h1d_jet_eec_rlsqrtpt_baseline[ipt]->SetName(Form("h1d_jet_eec_rlsqrtpt_%d", ipt));
-      h1d_jet_eec_rlsqrtpt_baseline[ipt]->Scale(1/h1d_jet_eec_rlsqrtpt_baseline[ipt]->Integral()); // normalization
+      h1d_jet_eec_rlsqrtpt_baseline[ipt]->SetName(Form("h1d_jet_eec_rlsqrtpt_%d_baseline", ipt));
     }
 
     ratio_hists(out_dir);
