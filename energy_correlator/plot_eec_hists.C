5029ba1010431520a073b798a8e7dce1b5014b5a
Kyle Devereaux <devereauxk@berkeley.edu>
2022-11-10 13:18:00 -0800

    new kyle suggestion - relative normalization

--- energy_correlator/plot_eec_hists.C
+++ energy_correlator/plot_eec_hists.C
@@ -307,8 +307,8 @@ void ratio_hists(const char* out_dir)
     {
       // calculate ratio
       TH1D* ratio = (TH1D*) h1d_jet_eec_rlsqrtpt[ipt]->Clone("ratio");
-      int norm_binrange_lo = h1d_jet_eec_rlsqrtpt->FindFixBin(1E-2);
-      int norm_binrange_hi = h1d_jet_eec_rlsqrtpt->FindFixBin(1E-1);
+      int norm_binrange_lo = h1d_jet_eec_rlsqrtpt[ipt]->FindFixBin(1E-2);
+      int norm_binrange_hi = h1d_jet_eec_rlsqrtpt[ipt]->FindFixBin(1E-1);
       double relative_normalization =  h1d_jet_eec_rlsqrtpt_baseline[ipt]->Integral(norm_binrange_lo,norm_binrange_hi) / h1d_jet_eec_rlsqrtpt[ipt]->Integral(norm_binrange_lo,norm_binrange_hi);
       cout<<"tot integral: "<<h1d_jet_eec_rlsqrtpt[ipt]->Integral()<<endl;
       cout<<"tot integral baseline: "<<h1d_jet_eec_rlsqrtpt_baseline[ipt]->Integral()<<endl;
