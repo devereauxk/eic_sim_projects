86e1cf1a2c0e203af553726869bc2ab81d9ba42f
Kyle Devereaux <devereauxk@berkeley.edu>
2022-11-13 18:47:57 -0800

    making and analyzing k=0 case for eC and eCu

--- energy_correlator/plot_eec_hists.C
+++ energy_correlator/plot_eec_hists.C
@@ -386,6 +386,53 @@ void ratio_hists(const char* out_dir)
     gROOT->ProcessLine( Form("cc%d->Print(\"%sh1d_jet_eec_rlsqrtpt_ratio_shifted_relnorm.pdf\")", cno-1, out_dir) );
   }
 
+  mclogx(cno++);
+  {
+    float plot_xrange_lo = 1E-2;
+    float plot_xrange_hi = 1;
+    float plot_yrange_lo = -0.05;
+    float plot_yrange_hi = 0.03;
+
+    TLegend* leg = new TLegend(0.21,0.7,0.51,0.82);
+    leg->SetBorderSize(0);
+    leg->SetTextSize(0.025);
+    leg->SetFillStyle(0);
+    leg->SetMargin(0.1);
+
+    for (int ipt = 0; ipt < ptbin-2; ipt++)
+    {
+      // calculate ratio
+      TH1D* ratio = (TH1D*) h1d_jet_eec[ipt]->Clone("ratio");
+      int norm_binrange_lo = h1d_jet_eec[ipt]->FindBin(1E-2);
+      int norm_binrange_hi = h1d_jet_eec[ipt]->FindBin(1E-1);
+      cout<<"hi bin high edge "<<h1d_jet_eec[ipt]->GetBinCenter(norm_binrange_hi) + h1d_jet_eec[ipt]->GetBinWidth(norm_binrange_hi)<<endl;
+      double relative_normalization =  h1d_jet_eec_baseline[ipt]->Integral(norm_binrange_lo,norm_binrange_hi) / h1d_jet_eec[ipt]->Integral(norm_binrange_lo,norm_binrange_hi);
+      ratio->Scale(relative_normalization);
+      ratio->Add(h1d_jet_eec_baseline[ipt], -1);
+      ratio->Scale(1/h1d_jet_eec_baseline[ipt]->Integral());
+
+      // plot
+      ratio->GetXaxis()->SetRangeUser(plot_xrange_lo,plot_xrange_hi);
+      ratio->GetYaxis()->SetRangeUser(plot_yrange_lo,plot_yrange_hi);
+      ratio->GetXaxis()->SetTitle("R_{L}");
+      ratio->GetYaxis()->SetTitle("normalized EEC (rel. norm. * on - off)");
+      ratio->SetMarkerColor(pt_color[ipt]);
+      ratio->SetLineColor(pt_color[ipt]);
+      ratio->SetMarkerSize(0.5);
+      ratio->SetMarkerStyle(21);
+      ratio->Draw("same hist");
+      leg->AddEntry(ratio,Form("%.1f GeV < p_{T} < %.1f GeV",pt_lo[ipt],pt_hi[ipt]));
+    }
+    leg->Draw("same");
+
+    TLine l1(plot_xrange_lo,0,plot_xrange_hi,0);
+    l1.SetLineStyle(7);
+    l1.SetLineColor(kGray+2);
+    l1.Draw("same");
+
+    gROOT->ProcessLine( Form("cc%d->Print(\"%sh1d_jet_eec_ratio_shifted_relnorm.pdf\")", cno-1, out_dir) );
+  }
+
   // ratio hists for h1d_jet_eec_rlsqrtpt, (relative normalization * on - off) / int dR_L off
   // Determine “relative normalization” by making sure that K=10 and K=0 are on top of each other in the region where we know there is no modification (small angle region).
   // small angle region determined by finding the x-intercept od the normalizaed on / off plot
--- energy_correlator/run_eec_hists_eCu.sh
+++ energy_correlator/run_eec_hists_eCu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-IN_DIR=/eic/u/kdevereaux/work/eHIJING/eHIJING-pythia/eHIJING-examples/Events/eCu_1E8_K4
-OUT_DIR=/eic/u/kdevereaux/work/energy_correlator/eHIJING/eCu_1E8_K4
+IN_DIR=/eic/u/kdevereaux/work/eHIJING/eHIJING-pythia/eHIJING-examples/Events/eCu_1E8_K0
+OUT_DIR=/eic/u/kdevereaux/work/energy_correlator/eHIJING/eCu_1E8_K0
 
 if [ -z "$1" ]
 then
