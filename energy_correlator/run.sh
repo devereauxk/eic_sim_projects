d3d263995ed245bb16c3efe40912f713cecfbd3c
Kyle Devereaux <devereauxk@berkeley.edu>
2022-11-13 20:43:17 -0800

    k=4 pow2

--- energy_correlator/run.sh
+++ energy_correlator/run.sh
@@ -6,7 +6,9 @@ root -l -q "plot_eec_hists.C(\"./eHIJING/eAu_1E8_K2/merged.root\", \"./eHIJING/e
 
 root -l -q "plot_eec_hists.C(\"./eHIJING/eAu_1E8_K4/merged.root\", \"./eHIJING/eAu_1E8_K4/\", 1, \"./eHIJING/eAu_1E8_K0_condor_v2/merged.root\")"
 
-#root -l -q "plot_eec_hists.C(\"./eHIJING/eAu_1E8_K4_pow2/merged.root\", \"./eHIJING/eAu_1E8_K4_pow2/\", 1, \"./eHIJING/eAu_1E8_K0_condor_v2_pow2/merged.root\")"
+root -l -q "plot_eec_hists.C(\"./eHIJING/eAu_1E8_K4_condor_v2_pow2/merged.root\", \"./eHIJING/eAu_1E8_K4_condor_v2_pow2/\")"
+
+root -l -q "plot_eec_hists.C(\"./eHIJING/eAu_1E8_K4_pow2/merged.root\", \"./eHIJING/eAu_1E8_K4_pow2/\", 1, \"./eHIJING/eAu_1E8_K4_condor_v2_pow2/merged.root\")"
 
 root -l -q "plot_eec_hists.C(\"./eHIJING/eC_1E8_K4/merged.root\", \"./eHIJING/eC_1E8_K4/\", 1, \"./eHIJING/eC_1E8_K0/merged.root\")"
 
