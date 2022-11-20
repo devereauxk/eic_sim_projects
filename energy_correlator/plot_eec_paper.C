4e2d1c46e00cb83fe2185ea1a23ad805b5b7dd5c
Kyle Devereaux <devereauxk@berkeley.edu>
2022-11-19 17:08:06 -0800

    plotttttingggg

--- energy_correlator/plot_eec_paper.C
+++ energy_correlator/plot_eec_paper.C
@@ -68,7 +68,7 @@ void pt_eta_3by3_hists()
           int norm_binrange_hi = temp->FindBin(0.05);
           double relative_normalization =  temp_baseline->Integral(norm_binrange_lo,norm_binrange_hi) / temp->Integral(norm_binrange_lo,norm_binrange_hi);
           temp->Scale(relative_normalization);
-          //temp->Scale(1/temp_baseline->Integral());
+          temp->Scale(1/temp_baseline->Integral());
 
           // plot histogram
           temp->GetXaxis()->SetRangeUser(plot_xrange_lo,plot_xrange_hi);
