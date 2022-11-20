e91ec4cda7ccb3d58fa8937e47139a4c279501fc
Kyle Devereaux <devereauxk@berkeley.edu>
2022-11-20 09:54:57 -0800

    plotsotostos

--- energy_correlator/plot_eec_paper.C
+++ energy_correlator/plot_eec_paper.C
@@ -27,10 +27,16 @@ static char* fname_eCu_by_K[knum] = {(char*)"./eHIJING/eCu_1E8_K0/merged.root", 
 static char* fname_eAu_by_K[knum] = {(char*)"./eHIJING/eAu_1E8_K0_condor_v2/merged.root", (char*)"./eHIJING/eAu_1E8_K2/merged.root", (char*)"./eHIJING/eAu_1E8_K4/merged.root", (char*)"./eHIJING/eAu_1E8_condor_v2/merged.root"};
 static char** fname_eA_by_K[speciesnum] = {fname_eC_by_K, fname_eCu_by_K, fname_eAu_by_K};
 
+static char* fname_eAu_by_E_K4[energynum] = {(char*)"./eHIJING/eAu_5_41_K4/merged.root", (char*)"./eHIJING/eAu_1E8_K4/merged.root", (char*)"./eHIJING/eAu_18_110_K4/merged.root"};
+static char* fname_eAu_by_E_K0[energynum] = {(char*)"./eHIJING/eAu_5_41_K0/merged.root", (char*)"./eHIJING/eAu_1E8_K0_condor_v2/merged.root", (char*)"./eHIJING/eAu_18_110_K0/merged.root"};
+
 const char* out_dir = "./paperplots/";
 
 TH1D* h1d_jet_eec[speciesnum][knum][etabin][ptbin] = {};
 TH1D* h1d_jet_eec_rlsqrtpt[speciesnum][knum][ptbin] = {};
+
+TH1D* h1d_jet_eec_eAu_by_E[energynum][knum][etabin][ptbin] = {};
+TH1D* h1d_jet_eec_rlsqrtpt_eAu_by_E[energynum][knum][ptbin] = {};
 
 static int cno = 0;
 
@@ -114,8 +120,8 @@ void nuclei_hists()
     float plot_xrange_hi = 1;
     float plot_yrange_lo = -0.015;
     float plot_yrange_hi = 0.04;
-    float legend_x = 0.25;
-    float legend_y = 0.5;
+    float legend_x = 0.22;
+    float legend_y = 0.6;
 
     TLegend* leg = new TLegend(legend_x,legend_y,legend_x+0.3,legend_y+0.15);
     leg->SetBorderSize(0);
@@ -171,6 +177,76 @@ void nuclei_hists()
   }
 }
 
+void energy_hists()
+{
+  // with R_L on the x-axis, plotting (alpha_i * K=i - K=0) / (int R_L K=0)
+  int k_pick = 2;
+  int ptbin_pick = 2;
+  int etabin_pick = 2;
+
+  mclogx(cno++);
+  {
+    float plot_xrange_lo = 1E-2;
+    float plot_xrange_hi = 1;
+    float plot_yrange_lo = -0.015;
+    float plot_yrange_hi = 0.04;
+    float legend_x = 0.22;
+    float legend_y = 0.6;
+
+    TLegend* leg = new TLegend(legend_x,legend_y,legend_x+0.3,legend_y+0.15);
+    leg->SetBorderSize(0);
+    leg->SetTextSize(0.028);
+    leg->SetFillStyle(0);
+    leg->SetMargin(0.1);
+
+    TH1D* temp;
+    TH1D* temp_baseline;
+
+    for (int ienergy = 0; ienergy < energynum; ienergy++)
+    {
+      temp = (TH1D*) h1d_jet_eec_eAu_by_E[ienergy][k_pick][etabin_pick][ptbin_pick]->Clone();
+      temp_baseline = (TH1D*) h1d_jet_eec_eAu_by_E[ienergy][0][etabin_pick][ptbin_pick]->Clone();
+
+      // calculate relative normalization ratio
+      int norm_binrange_lo = temp->FindBin(1E-3);
+      int norm_binrange_hi = temp->FindBin(0.2);
+      double relative_normalization =  temp_baseline->Integral(norm_binrange_lo,norm_binrange_hi) / temp->Integral(norm_binrange_lo,norm_binrange_hi);
+      temp->Scale(relative_normalization);
+      temp->Add(temp_baseline, -1);
+      temp->Scale(1/temp_baseline->Integral());
+
+      // plot
+      temp->GetXaxis()->SetRangeUser(plot_xrange_lo,plot_xrange_hi);
+      temp->GetYaxis()->SetRangeUser(plot_yrange_lo,plot_yrange_hi);
+      temp->GetXaxis()->SetTitle("R_{L}");
+      temp->GetYaxis()->SetTitle("normalized EEC (rel. norm. * on - off)");
+      temp->SetMarkerColor(pt_color[ispecies]);
+      temp->SetLineColor(pt_color[ispecies]);
+      temp->SetMarkerSize(0.5);
+      temp->SetMarkerStyle(21);
+      temp->Draw("same hist");
+      leg->AddEntry(temp,energy[ienergy]);
+    }
+    leg->Draw("same");
+
+    TLine l1(plot_xrange_lo,0,plot_xrange_hi,0);
+    l1.SetLineStyle(7);
+    l1.SetLineColor(kGray+2);
+    l1.Draw("same");
+
+    TLatex* tl = new TLatex();
+    tl->SetTextAlign(11);
+    tl->SetTextSize(0.028);
+    tl->SetTextColor(kBlack);
+    tl->DrawLatexNDC(0.22,0.84,"eHIJING, e+Au @ 10+100 GeV, 10^{8} events");
+    tl->DrawLatexNDC(0.22,0.81,Form("#eta #in [%.1f, %0.1f)",eta_lo[etabin_pick],eta_hi[etabin_pick]));
+    tl->DrawLatexNDC(0.22,0.78,Form("p_{T,jet} #in [%.1f, %0.1f)",pt_lo[ptbin_pick],pt_hi[ptbin_pick]));
+
+    gROOT->ProcessLine( Form("cc%d->Print(\"%sh1d_jet_eec_by_energy.pdf\")", cno-1, out_dir) );
+
+  }
+}
+
 void plot_eec_paper()
 {
 
@@ -214,13 +290,76 @@ void plot_eec_paper()
     }
   }
 
+  for (int ienergy = 0; ienergy < energynum; ienergy++)
+  {
+    // K0
+    fin_name = fname_eAu_by_E_K0[ienergy];
+
+    if (strcmp(fin_name,"") != 0)
+    {
+      fin = new TFile(fin_name, "READ");
+
+      for (int ieta = 0; ieta < etabin; ieta++)
+      {
+        for (int ipt = 0; ipt < ptbin; ipt++)
+        {
+          // raw data histograms
+          h1d_jet_eec_eAu_by_E[ienergy][0][ieta][ipt] = (TH1D*) fin->Get(Form("h1d_jet_eec_%d_%d", ieta, ipt));
+          h1d_jet_eec_eAu_by_E[ienergy][0][ieta][ipt]->SetName(Form("h1d_jet_eec_eAubyE_%d_%d_%d_%d", ieta, ipt, ispecies, ik));
+        }
+      }
+      for (int ipt = 0; ipt < ptbin; ipt++)
+      {
+        // raw data histogram
+        h1d_jet_eec_rlsqrtpt_eAu_by_E[ispecies][0][ipt] = (TH1D*) fin->Get(Form("h1d_jet_eec_rlsqrtpt_%d", ipt));
+        h1d_jet_eec_rlsqrtpt_eAu_by_E[ispecies][0][ipt]->SetName(Form("h1d_jet_eec_rlsqrtpt_eAubyE_%d_%d_%d", ipt, ispecies, ik));
+      }
+      cout<<fin_name<<" loaded!"<<endl;
+    }
+    else
+    {
+      cout<<"couldn't find file for "<<energy[ienergy]<<endl;
+    }
+
+    // K4
+    fin_name = fname_eAu_by_E_K4[ienergy];
+
+    if (strcmp(fin_name,"") != 0)
+    {
+      fin = new TFile(fin_name, "READ");
+
+      for (int ieta = 0; ieta < etabin; ieta++)
+      {
+        for (int ipt = 0; ipt < ptbin; ipt++)
+        {
+          // raw data histograms
+          h1d_jet_eec_eAu_by_E[ienergy][2][ieta][ipt] = (TH1D*) fin->Get(Form("h1d_jet_eec_%d_%d", ieta, ipt));
+          h1d_jet_eec_eAu_by_E[ienergy][2][ieta][ipt]->SetName(Form("h1d_jet_eec_eAubyE_%d_%d_%d_%d", ieta, ipt, ispecies, ik));
+        }
+      }
+      for (int ipt = 0; ipt < ptbin; ipt++)
+      {
+        // raw data histogram
+        h1d_jet_eec_rlsqrtpt_eAu_by_E[ispecies][2][ipt] = (TH1D*) fin->Get(Form("h1d_jet_eec_rlsqrtpt_%d", ipt));
+        h1d_jet_eec_rlsqrtpt_eAu_by_E[ispecies][2][ipt]->SetName(Form("h1d_jet_eec_rlsqrtpt_eAubyE_%d_%d_%d", ipt, ispecies, ik));
+      }
+      cout<<fin_name<<" loaded!"<<endl;
+    }
+    else
+    {
+      cout<<"couldn't find file for "<<energy[ienergy]<<endl;
+    }
+
+
+  }
+
   // plot individual panels
 
   pt_eta_3by3_hists();
 
   nuclei_hists();
 
-  //energy_hists();
+  energy_hists();
 
   //power_hists();
 
