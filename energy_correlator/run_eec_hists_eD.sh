3fd5c56bb3b38d1727e29a835bf31a06abba344f
Kyle Devereaux <devereauxk@berkeley.edu>
2022-11-27 14:50:57 -0800

    eD comparisson

---
+++ energy_correlator/run_eec_hists_eD.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+IN_DIR=/eic/u/kdevereaux/work/eHIJING/eHIJING-pythia/eHIJING-examples/Events/eD_1E8_K0
+OUT_DIR=/eic/u/kdevereaux/work/energy_correlator/eHIJING/eD_1E8_K0
+
+if [ -z "$1" ]
+then
+        echo "No job number set."
+        echo "Please run as ./run_ep.sh jobnumber"
+        echo "Exiting..."
+        exit 1
+fi
+
+#Go into scratch directory
+chmod g+rx ${_CONDOR_SCRATCH_DIR}
+cd ${_CONDOR_SCRATCH_DIR}
+
+#Make subdirectory and move there
+INPUT=$(( 0 + $1 ))
+echo $INPUT
+DIR=`printf "%04d" $INPUT`
+mkdir $DIR
+
+ln -s $IN_DIR/ep_${INPUT}.dat
+ln -s /eic/u/kdevereaux/work/energy_correlator/eec_hists.C
+
+mkdir $OUT_DIR
+
+root -l -b -q "eec_hists.C(\"ep_${INPUT}.dat\",\"$OUT_DIR/hists_eec_${INPUT}.root\", 1, 2130.17, 100, 4, 1)"
