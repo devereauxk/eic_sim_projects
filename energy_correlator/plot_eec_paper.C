c052435404e0f10b695420988ee261e511057b67
Kyle Devereaux <devereauxk@berkeley.edu>
2022-11-28 17:08:24 -0800

    revising paper plots

--- energy_correlator/plot_eec_paper.C
+++ energy_correlator/plot_eec_paper.C
@@ -325,8 +325,8 @@ void pt_bin_side_by_side()
       temp->GetYaxis()->SetRangeUser(plot_yrange_lo,plot_yrange_hi);
       temp->GetXaxis()->SetTitle("R_{L}#sqrt{p_{T,jet}}");
       temp->GetYaxis()->SetTitle("normalized EEC (rel. norm. * on - off)");
-      temp->SetMarkerColor(pt_color[ispecies]);
-      temp->SetLineColor(pt_color[ispecies]);
+      temp->SetMarkerColor(pt_color[ipt]);
+      temp->SetLineColor(pt_color[ipt]);
       temp->SetMarkerSize(0.5);
       temp->SetMarkerStyle(21);
       temp->Draw("same hist");
