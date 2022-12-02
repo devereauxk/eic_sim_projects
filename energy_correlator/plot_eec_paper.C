42dc05dbf678563ab9baa03fa1dc368b34c42f24
Kyle Devereaux <devereauxk@berkeley.edu>
2022-12-01 22:03:24 -0800

    ep baseline for all plots in plot_eec_paper

--- energy_correlator/plot_eec_paper.C
+++ energy_correlator/plot_eec_paper.C
@@ -481,7 +481,7 @@ void nuclei_hists()
       temp->SetLineColor(pt_color[ispecies]);
       temp->SetMarkerSize(0.5);
       temp->SetMarkerStyle(21);
-      temp->Draw("same hist");
+      temp->Draw("same e");
       leg->AddEntry(temp,Form("%s, K = %i",species[ispecies], k[k_pick]));
     }
     leg->Draw("same");
