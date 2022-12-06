1bdc1817a7c16688abd200120a8e5ee6f422b5cd
Kyle Devereaux <devereauxk@berkeley.edu>
2022-12-05 21:00:47 -0800

    more plots

--- energy_correlator/plot_eec_hists.C
+++ energy_correlator/plot_eec_hists.C
@@ -493,7 +493,7 @@ void ratio_hists(const char* out_dir)
       ratio->GetXaxis()->SetRangeUser(plot_xrange_lo,plot_xrange_hi);
       ratio->GetYaxis()->SetRangeUser(plot_yrange_lo,plot_yrange_hi);
       ratio->GetXaxis()->SetTitle("R_{L}");
-      ratio->GetYaxis()->SetTitle("%% deviation from baseline");
+      ratio->GetYaxis()->SetTitle("% deviation from baseline");
       ratio->SetMarkerColor(pt_color[ipt]);
       ratio->SetLineColor(pt_color[ipt]);
       ratio->SetMarkerSize(0.5);
