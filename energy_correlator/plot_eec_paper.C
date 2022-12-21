8912dc5a1ec914174f75032783b369e38b3db13a
Kyle Devereaux <devereauxk@berkeley.edu>
2022-12-21 14:37:07 -0800

    plotting mods

--- energy_correlator/plot_eec_paper.C
+++ energy_correlator/plot_eec_paper.C
@@ -58,7 +58,7 @@ static char** fname_eAu_by_E[knum] = {fname_eAu_by_E_K0, NULL, fname_eAu_by_E_K4
 static char* fname_ep_by_power[pownum] = {(char*)"./eHIJING/ep_10_100_K0_pow05/merged.root", (char*)"./eHIJING/ep_10_100_K0/merged.root", (char*)"./eHIJING/ep_10_100_K0_pow15/merged.root", (char*)"./eHIJING/ep_10_100_K0_pow2/merged.root"}; // all cases are K=0, 2E8 events
 static char* fname_eAu_by_power[pownum] = {(char*)"./eHIJING/eAu_10_100_K4_pow05/merged.root", (char*)"./eHIJING/eAu_10_100_K4/merged.root", (char*)"./eHIJING/eAu_10_100_K4_pow15/merged.root", (char*)"./eHIJING/eAu_10_100_K4_pow2/merged.root"}; // all cases are K=4, 2E8 events
 
-const char* out_dir = "./test/";
+const char* out_dir = "./paperplots/";
 
 TH1D* h1d_jet_pt[speciesnum] = {}; // K=4, 10x100, 2E8 events
 
@@ -1510,7 +1510,7 @@ void peak_height_vs_A()
 
     //g->GetXaxis()->SetRangeUser(plot_xrange_lo,plot_xrange_hi);
     //g->GetYaxis()->SetRangeUser(plot_yrange_lo,plot_yrange_hi);
-    g->GetXaxis()->SetTitle("ln(A)");
+    g->GetXaxis()->SetTitle("log(A)");
     g->GetYaxis()->SetTitle("normalized EEC value @ R_{L}=1");
     g->SetMarkerColor(pt_color[0]);
     g->SetLineColor(pt_color[0]);
