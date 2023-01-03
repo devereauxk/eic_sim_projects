73379caa63d1ac4ca7dad3f3846205de7f737336
Kyle Devereaux <devereauxk@berkeley.edu>
2023-01-03 00:06:32 -0700

    Q2-x analysis

--- energy_correlator/plot_eec_paper.C
+++ energy_correlator/plot_eec_paper.C
@@ -1081,7 +1081,7 @@ void Q2_x_panel()
   {
     for (int ipt = 0; ipt < ptbin; ipt++)
     {
-      mclogxy(cno++, 0, 0, 400, 400, 0.1, 0.1, 0.1, 0.13);
+      mclogxy(cno++, 0, 0, 400, 400, 0.12, 0.15, 0.1, 0.13);
       {
         float plot_xrange_lo = 0.8;
         float plot_xrange_hi = 2.7;
