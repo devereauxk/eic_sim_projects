ffaecf3aaec3b145dade800617292990e946b78c
Kyle Devereaux <devereauxk@berkeley.edu>
2023-01-19 12:56:36 -0800

    pythia8 Q2x distros

--- energy_correlator/run_Q2_x_ep_pythia.sh
+++ energy_correlator/run_Q2_x_ep_pythia.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-IN_DIR=/eic/u/kdevereaux/work/reconstruction/ep_10_100/outfiles
-OUT_DIR=/eic/u/kdevereaux/work/energy_correlator/eHIJING/ep_10_100_Q2x_pythia
+#IN_DIR=/eic/u/kdevereaux/work/reconstruction/ep_10_100/outfiles
+#OUT_DIR=/eic/u/kdevereaux/work/energy_correlator/eHIJING/ep_10_100_Q2x_pythia
+IN_DIR=/gpfs/mnt/gpfs02/eic/wfan/data/pythia8HepMC_e10p100_MB
+OUT_DIR=/eic/u/kdevereaux/work/energy_correlator/eHIJING/ep_10_100_Q2x_pythia8
 
 if [ -z "$1" ]
 then
@@ -20,11 +22,13 @@ echo $INPUT
 DIR=`printf "%04d" $INPUT`
 mkdir $DIR
 
-ln -s $IN_DIR/ep_${INPUT}.root
+#ln -s $IN_DIR/ep_${INPUT}.root
+ln -s $IN_DIR/ep_minbias_highQ2_${INPUT}.root
 ln -s /eic/u/kdevereaux/work/energy_correlator/Q2_x.C
 
 mkdir $OUT_DIR
 
-root -l -b -q "Q2_x.C(\"ep_${INPUT}.root\",\"$OUT_DIR/hists_Q2_x_${INPUT}.root\", 0)"
+#root -l -b -q "Q2_x.C(\"ep_${INPUT}.root\",\"$OUT_DIR/hists_Q2_x_${INPUT}.root\", 0)"
+root -l -b -q "Q2_x.C(\"ep_minbias_highQ2_${INPUT}.root\",\"$OUT_DIR/hists_Q2_x_${INPUT}.root\", 0)"
 
 #root -l -q "Q2_x.C(\"/eic/u/kdevereaux/work/reconstruction/ep_10_100/outfiles/ep_1.root\",\"./test.root\", 0)"
