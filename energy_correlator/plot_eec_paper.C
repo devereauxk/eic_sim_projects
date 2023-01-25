2cbb10dacea6506144578c6b29783a3af1762a58
Kyle Devereaux <devereauxk@berkeley.edu>
2023-01-24 19:11:32 -0800

    norming raw nuclei distro by jet rate

--- energy_correlator/plot_eec_paper.C
+++ energy_correlator/plot_eec_paper.C
@@ -848,6 +848,7 @@ void nuclei_hists()
     temp = (TH1D*) h1d_jet_eec_rlsqrtpt[0][0][etabin_pick][ptbin_pick]->Clone();
     temp->Add(temp,-1);
     temp->GetXaxis()->SetRangeUser(plot_xrange_lo,plot_xrange_hi);
+    temp->GetYaxis()->SetRangeUser(plot_yrange_lo,plot_yrange_hi);
     temp->GetXaxis()->SetTitle("R_{L}#sqrt{p_{T,jet}}");
     temp->GetYaxis()->SetTitle("EEC self-normalized by jet rate (eA - ep)");
     temp->SetMarkerColor(pt_color[0]);
@@ -868,6 +869,7 @@ void nuclei_hists()
 
       // plot
       temp->GetXaxis()->SetRangeUser(plot_xrange_lo,plot_xrange_hi);
+      temp->GetYaxis()->SetRangeUser(plot_yrange_lo,plot_yrange_hi);
       temp->GetXaxis()->SetTitle("R_{L}#sqrt{p_{T,jet}}");
       temp->GetYaxis()->SetTitle("EEC self-normalized by jet rate (eA - ep)");
       temp->SetMarkerColor(pt_color[i+1]);
