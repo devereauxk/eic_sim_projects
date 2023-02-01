0faca6a8093c45db48023cc54a5c7b50b612d782
Kyle Devereaux <devereauxk@berkeley.edu>
2023-01-31 21:38:48 -0800

    adding -1, 0 and 0, 1 eta bins to eec_hists

--- energy_correlator/run_eec_hists_ep.sh
+++ energy_correlator/run_eec_hists_ep.sh
@@ -27,4 +27,4 @@ mkdir $OUT_DIR
 
 root -l -b -q "eec_hists.C(\"ep_${INPUT}.dat\",\"$OUT_DIR/hists_eec_${INPUT}.root\", 1, 2131.56, 100, 0, 0.5, 0, 0)"
 
-#root -l -b -q "eec_hists.C(\"/eic/u/kdevereaux/work/eHIJING/eHIJING-pythia/eHIJING-examples/Events/ep_10_100_K0/ep_1.dat\",\"temp.root\", 1, 2131.56, 100, 0, 0.5, 0)"
+#root -l -b -q "eec_hists.C(\"/eic/u/kdevereaux/work/eHIJING/eHIJING-pythia/eHIJING-examples/Events/ep_10_100_K0/ep_1.dat\",\"temp.root\", 1, 2131.56, 100, 0, 0.5, 0, 0)"
