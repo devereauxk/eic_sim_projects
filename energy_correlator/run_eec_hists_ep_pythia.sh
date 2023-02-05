69d15f1e3537fc1461c288a19b1ec6e433bc6791
Kyle Devereaux <devereauxk@berkeley.edu>
2023-02-04 20:13:37 -0800

    eCa preped, ep pythia8 lab frame ready

--- energy_correlator/run_eec_hists_ep_pythia.sh
+++ energy_correlator/run_eec_hists_ep_pythia.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-IN_DIR=/gpfs/mnt/gpfs02/eic/wfan/data/pythia8HepMC_e10p100_ft_MB
-OUT_DIR=/eic/u/kdevereaux/work/energy_correlator/eHIJING/ep_10_100_pythia8_ft_noboost
+IN_DIR=/gpfs/mnt/gpfs02/eic/wfan/data/pythia8HepMC_e10p100_MB
+OUT_DIR=/eic/u/kdevereaux/work/energy_correlator/eHIJING/ep_10_100_pythia8
 
 if [ -z "$1" ]
 then
@@ -20,11 +20,12 @@ echo $INPUT
 DIR=`printf "%04d" $INPUT`
 mkdir $DIR
 
-ln -s $IN_DIR/hepmcout-${INPUT}.root
+#ln -s $IN_DIR/hepmcout-${INPUT}.root
+ln -s $IN_DIR/ep_minbias_highQ2_${INPUT}.root
 ln -s /eic/u/kdevereaux/work/energy_correlator/eec_hists.C
 
 mkdir $OUT_DIR
 
-root -l -b -q "eec_hists.C(\"hepmcout-${INPUT}.root\",\"$OUT_DIR/hists_eec_${INPUT}.root\", -1, 2131, 100, 0, 0.5, 0, 1)"
+root -l -b -q "eec_hists.C(\"ep_minbias_highQ2_${INPUT}.root\",\"$OUT_DIR/hists_eec_${INPUT}.root\", -1, 2131, 100, 0, 0.5, 0, 1)"
 
 #root -l -b -q "eec_hists.C(\"/gpfs/mnt/gpfs02/eic/wfan/data/pythia8HepMC_e10p100_ft_MB/hepmcout-1.root\",\"temp.root\", -1, 2131, 100, 0, 0.5, 1, 1)"
