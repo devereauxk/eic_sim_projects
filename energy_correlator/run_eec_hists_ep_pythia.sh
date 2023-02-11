3f1c514ecf49550c3ea936ad26e494a7caca17b9
Kyle Devereaux <devereauxk@berkeley.edu>
2023-02-10 18:41:37 -0800

    emergenecy eec recalculation

--- energy_correlator/run_eec_hists_ep_pythia.sh
+++ energy_correlator/run_eec_hists_ep_pythia.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 IN_DIR=/gpfs/mnt/gpfs02/eic/wfan/data/pythia8HepMC_e10p100_ft_MB
-OUT_DIR=/eic/u/kdevereaux/work/energy_correlator/eHIJING/ep_10_100_pythia8_ft_noboost
+OUT_DIR=/eic/u/kdevereaux/work/energy_correlator/eHIJING/ep_10_100_pythia8_ft_boosted
 
 if [ -z "$1" ]
 then
@@ -28,6 +28,6 @@ mkdir $OUT_DIR
 
 #                                                                                                        DOUBLE CHECK THESE
 #                                                                                                        power, boost, Q2x
-root -l -b -q "eec_hists.C(\"hepmcout-${INPUT}.root\",\"$OUT_DIR/hists_eec_${INPUT}.root\", -1, 2131, 100, 0, 0.5, 0, 1)"
+root -l -b -q "eec_hists.C(\"hepmcout-${INPUT}.root\",\"$OUT_DIR/hists_eec_${INPUT}.root\", -1, 2131, 100, 0, 0.5, 1, 1)"
 
 #root -l -b -q "eec_hists.C(\"/gpfs/mnt/gpfs02/eic/wfan/data/pythia8HepMC_e10p100_ft_MB/hepmcout-1.root\",\"temp.root\", -1, 2131, 100, 0, 0.5, 1, 1)"
