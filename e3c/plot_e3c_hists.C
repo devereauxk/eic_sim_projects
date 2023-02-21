297ebfa24f0346392639c83123d7e6f17f72471e
Kyle Devereaux <devereauxk@berkeley.edu>
2023-02-21 15:07:46 -0800

    plots

--- e3c/plot_e3c_hists.C
+++ e3c/plot_e3c_hists.C
@@ -253,7 +253,7 @@ void overlay_hists(const char* out_dir)
       float plot_xrange_lo = 0;
       float plot_xrange_hi = 1;
       float plot_yrange_lo = 0;
-      float plot_yrange_hi = 1.5; // TMath::Pi() / 2.0;
+      float plot_yrange_hi = TMath::Pi() / 2.0;
       float plot_zrange_lo = 0;
       float plot_zrange_hi = 0.006;
 
@@ -264,7 +264,7 @@ void overlay_hists(const char* out_dir)
       temp->Scale(1/temp->Integral());
 
       temp->GetXaxis()->SetRangeUser(plot_xrange_lo,plot_xrange_hi);
-      //temp->GetYaxis()->SetRangeUser(plot_yrange_lo,plot_yrange_hi);
+      temp->GetYaxis()->SetRangeUser(plot_yrange_lo,plot_yrange_hi);
       //temp->GetZaxis()->SetRangeUser(plot_zrange_lo, plot_zrange_hi);
       temp->GetXaxis()->SetTitle("#xi");
       temp->GetYaxis()->SetTitle("#phi");
