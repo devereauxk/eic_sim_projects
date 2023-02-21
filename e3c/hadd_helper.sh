e6e76212bbb4a3de1088113af9d9ce2bf24a3d03
Kyle Devereaux <devereauxk@berkeley.edu>
2023-02-21 14:31:50 -0800

    hadd_helper

--- e3c/hadd_helper.sh
+++ e3c/hadd_helper.sh
@@ -2,15 +2,15 @@ TARGET_DIR=$1
 
 cd $TARGET_DIR
 
-hadd -j merged_1.root *_1*
-hadd -j merged_2.root *_2*
-hadd -j merged_3.root *_3*
-hadd -j merged_4.root *_4*
-hadd -j merged_5.root *_5*
-hadd -j merged_6.root *_6*
-hadd -j merged_7.root *_7*
-hadd -j merged_8.root *_8*
-hadd -j merged_9.root *_9*
+hadd -j merged_temp_1.root *_1*
+hadd -j merged_temp_2.root *_2*
+hadd -j merged_temp_3.root *_3*
+hadd -j merged_temp_4.root *_4*
+hadd -j merged_temp_5.root *_5*
+hadd -j merged_temp_6.root *_6*
+hadd -j merged_temp_7.root *_7*
+hadd -j merged_temp_8.root *_8*
+hadd -j merged_temp_9.root *_9*
 
 hadd -j merged.root merged_temp_*
 
