175c0e1f083e34edddf0ca302e085ff6c9fe5a7f
Kyle Devereaux <devereauxk@berkeley.edu>
2023-02-20 17:20:02 -0800

    testing e3c

--- e3c/run.sh
+++ e3c/run.sh
@@ -1,1 +1,3 @@
 root -l -q "plot_e3c_hists.C(\"./analysis/ep_10_100_pythia8/merged.root\", \"./analysis/ep_10_100_pythia8/\")"
+
+root -l -q "plot_e3c_hists.C(\"./analysis/ep_10_100_K0_pow05/merged.root\", \"./analysis/ep_10_100_K0_pow05/\")"
