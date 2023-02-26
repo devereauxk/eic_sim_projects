c81aa5855f97bf15f0d513b1ae0549d45779f41e
Kyle Devereaux <devereauxk@berkeley.edu>
2023-02-25 20:49:15 -0800

    dev on plot_e3c_ratios

--- e3c/plot_e3c_ratios.C
+++ e3c/plot_e3c_ratios.C
@@ -17,7 +17,7 @@ const int speciesnum = 9;
 static char* species[speciesnum] = {(char*)"e+p", (char*)"e+D", (char*)"e+He-3", (char*)"e+He-4", (char*)"e+C", (char*)"e+Ca", (char*)"e+Cu", (char*)"e+Au", (char*)"e+U"};
 static int species_A[speciesnum] = {1, 2, 3, 4, 12, 40, 64, 197, 238};
 
-static char** fname_eA[speciesnum] = {(char*)"./analysis/ep_10_100_K0_pow05/merged.root", (char*)"", (char*)"", (char*)"", (char*)"", (char*)"", (char*)"", \
+static char* fname_eA[speciesnum] = {(char*)"./analysis/ep_10_100_K0_pow05/merged.root", (char*)"", (char*)"", (char*)"", (char*)"", (char*)"", (char*)"", \
   (char*)"./analysis/eAu_1E8_K4_pow05/merged.root", (char*)""};
 
 const char* out_dir = "./paperplots/";
