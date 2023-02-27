0e26c9168adf1889aada551193424435df88f7f0
Kyle Devereaux <devereauxk@berkeley.edu>
2023-02-26 17:41:43 -0800

    plot_e3c_ratio adding surface plots

--- e3c/plot_e3c_ratios.C
+++ e3c/plot_e3c_ratios.C
@@ -145,11 +145,11 @@ void e3c_projected_hists()
 
 }
 
-void xi_phi_ratio_hists()
+void xi_phi_difference_hists()
 {
   int species_pick = 7;
   int etabin_pick = 2;
-  int ptbin_pick = 2;
+  int ptbin_pick = 1;
   float rl_range_lo = 1E-2;
   float rl_range_hi = 1;
 
@@ -210,6 +210,84 @@ void xi_phi_ratio_hists()
       tl->DrawLatexNDC(0.22,0.84,Form("#eta #in [%.1f, %.1f), p_{T} #in [%.1f, %.1f)",eta_lo[etabin_pick],eta_hi[etabin_pick],pt_lo[ptbin_pick],pt_hi[ptbin_pick]));
       tl->DrawLatexNDC(0.22,0.81,Form("R_{L} ~ %.3f", bin_center));
 
+      gROOT->ProcessLine( Form("cc%d->Print(\"%sh2d_jet_e3c_xi_phi_diff_%d.pdf\")", cno-1, out_dir, ibin) );
+    }
+
+    mcs(cno++, 0, 0, 400, 400, 0.12, 0.15, 0.1, 0.13);
+    {
+      temp->Draw("SURF2Z");
+
+      gROOT->ProcessLine( Form("cc%d->Print(\"%sh2d_jet_e3c_xi_phi_diff_%d_surface.pdf\")", cno-1, out_dir, ibin) );
+    }
+
+  }
+
+}
+
+void xi_phi_ratio_hists()
+{
+  int species_pick = 7;
+  int etabin_pick = 1;
+  int ptbin_pick = 2;
+  float rl_range_lo = 1E-2;
+  float rl_range_hi = 1;
+
+  TH3D* picked = (TH3D*) h3d_jet_eec_rl_xi_phi[species_pick][etabin_pick][ptbin_pick]->Clone();
+  TH3D* baseline = (TH3D*) h3d_jet_eec_rl_xi_phi[0][etabin_pick][ptbin_pick]->Clone();
+
+  TH1D* picked_x = h3d_jet_eec_rl_xi_phi[species_pick][etabin_pick][ptbin_pick]->ProjectionX();
+  TH1D* baseline_x = h3d_jet_eec_rl_xi_phi[0][etabin_pick][ptbin_pick]->ProjectionX();
+
+  int norm_binrange_lo = picked_x->FindBin(rl_range_lo);
+  cout<<"low bin: "<<norm_binrange_lo<<endl;
+  int norm_binrange_hi = picked_x->FindBin(rl_range_hi);
+  cout<<"high bin: "<<norm_binrange_hi<<endl;
+  if (norm_binrange_lo == 0)
+  {
+    norm_binrange_lo = 1;
+    cout<<"bin range lo too low; set to 1"<<endl;
+  }
+  if (norm_binrange_hi > picked_x->GetNbinsX()+1)
+  {
+    norm_binrange_hi = picked_x->GetNbinsX()+1;
+    cout<<"bin range hi too high; set to "<<picked_x->GetNbinsX()<<endl;
+  }
+  double relative_normalization =  baseline_x->Integral(norm_binrange_lo,norm_binrange_hi) / picked_x->Integral(norm_binrange_lo,norm_binrange_hi);
+  picked->Scale(relative_normalization);
+  picked->Divide(relative_normalization);
+
+  TH3D* sliced;
+  TH2D* temp;
+  for (int ibin = norm_binrange_lo; ibin <= norm_binrange_hi; ibin++)
+  {
+    float plot_xrange_lo = 0;
+    float plot_xrange_hi = 1;
+    float plot_yrange_lo = 0;
+    float plot_yrange_hi = 1.5; // TMath::Pi() / 2.0;
+    float plot_zrange_lo = -0.1E-3;
+    float plot_zrange_hi = 0.3E-3;
+
+    float bin_center = picked->GetXaxis()->GetBinCenter(ibin);
+    sliced = (TH3D*) picked->Clone("temp3d");
+    sliced->GetXaxis()->SetRange(ibin,ibin);
+    temp = (TH2D*) sliced->Project3D("zy");
+
+    mcs(cno++, 0, 0, 400, 400, 0.12, 0.15, 0.1, 0.13);
+    {
+      temp->GetXaxis()->SetRangeUser(plot_xrange_lo,plot_xrange_hi);
+      temp->GetYaxis()->SetRangeUser(plot_yrange_lo,plot_yrange_hi);
+      //temp->GetZaxis()->SetRangeUser(plot_zrange_lo, plot_zrange_hi);
+      temp->GetXaxis()->SetTitle("#xi");
+      temp->GetYaxis()->SetTitle("#phi");
+      temp->Draw("colz");
+
+      TLatex* tl = new TLatex();
+      tl->SetTextAlign(11);
+      tl->SetTextSize(0.03);
+      tl->SetTextColor(kBlack);
+      tl->DrawLatexNDC(0.22,0.84,Form("#eta #in [%.1f, %.1f), p_{T} #in [%.1f, %.1f)",eta_lo[etabin_pick],eta_hi[etabin_pick],pt_lo[ptbin_pick],pt_hi[ptbin_pick]));
+      tl->DrawLatexNDC(0.22,0.81,Form("R_{L} ~ %.3f", bin_center));
+
       gROOT->ProcessLine( Form("cc%d->Print(\"%sh2d_jet_e3c_xi_phi_ratio_%d.pdf\")", cno-1, out_dir, ibin) );
     }
 
@@ -255,6 +333,8 @@ void plot_e3c_ratios()
 
   // make the plots
   e3c_projected_hists();
+
+  xi_phi_difference_hists();
 
   xi_phi_ratio_hists();
 
