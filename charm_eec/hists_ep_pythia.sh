794f3c208e596041fe4cf9680a03dc5b84d2c1d1
Kyle Devereaux <devereauxk@berkeley.edu>
2023-03-19 14:58:18 -0700

    debugging charm eec

--- charm_eec/hists_ep_pythia.sh
+++ charm_eec/hists_ep_pythia.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #IN_DIR=/gpfs/mnt/gpfs02/eic/wfan/data/pythia8HepMC_e10p100_ft_MB
 IN_DIR=/gpfs/mnt/gpfs02/eic/wfan/data/pythia8HepMC_e10p100_MB
-OUT_DIR=/eic/u/kdevereaux/work/charm_eec/analysis/ep_10_100_pythia8_D0jets
+OUT_DIR=/eic/u/kdevereaux/work/charm_eec/analysis/ep_10_100_pythia8_D0inpair
 
 if [ -z "$1" ]
 then
@@ -29,12 +29,13 @@ mkdir $OUT_DIR
 POW=$2
 DO_BOOST=$3
 DO_Q2X=$4
-FORCE_PART_INJET=$5
-FORCE_PART_INPAIR=$6
+DO_FORCE_INJET=$5
+DO_FORCE_INPAIR=$6
+FIXED_ID=$7
 
 #                                                                                                        DOUBLE CHECK THESE
 #                                                                                                                            pow,boost,Q2x,jet,pair
-root -l -b -q "charm_eec_hists.C(\"ep_minbias_highQ2_${INPUT}.root\",\"$OUT_DIR/hists_eec_${INPUT}.root\", -1, 2131, 100, 0, $POW, $DO_BOOST, $DO_Q2X, $FORCE_PART_INJET, $FORCE_PART_INPAIR)"
+root -l -b -q "charm_eec_hists.C(\"ep_minbias_highQ2_${INPUT}.root\",\"$OUT_DIR/hists_eec_${INPUT}.root\", -1, 2131, 100, 0, $POW, $DO_BOOST, $DO_Q2X, $DO_FORCE_INJET, $DO_FORCE_INPAIR, $FIXED_ID)"
 #root -l -b -q "eec_hists.C(\"hepmcout-${INPUT}.root\",\"$OUT_DIR/hists_eec_${INPUT}.root\", -1, 2131, 100, 0, 0.5, 1, 1)"
 
 #root -l -b -q "charm_eec_hists.C(\"/gpfs/mnt/gpfs02/eic/wfan/data/pythia8HepMC_e10p100_MB/ep_minbias_highQ2_99.root\",\"temp.root\", -1, 2131, 100, 0, 0.5, 0, 1, 1, 1, 421)"
