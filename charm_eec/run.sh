4865e01fddb5dbbf9d59a0255fc470482df1c81d
Kyle Devereaux <devereauxk@berkeley.edu>
2023-04-04 23:51:24 -0700

    charm eec

--- charm_eec/run.sh
+++ charm_eec/run.sh
@@ -1,14 +1,14 @@
 
-INJET_DIR=./analysis/ep_10_100_pythia8_pow1_D0injet
+DIR=./analysis/ep_10_100_pythia8_pow1_D0injet
 
-rm $INJET_DIR/merged.root
-hadd -j $INJET_DIR/merged.root $INJET_DIR/*.root
-root -l -q "plot_charm_eec_hists.C(\"$INJET_DIR/merged.root\", \"$INJET_DIR/\")"
+rm $DIR/merged.root
+hadd -j $DIR/merged.root $DIR/*.root
+root -l -q "plot_charm_eec_hists.C(\"$DIR/merged.root\", \"$DIR/\")"
 
-INC_DIR=./analysis/ep_10_100_pythia8_pow1_inclusive
+DIR=./analysis/ep_10_100_pythia8_pow1_inclusive
 
-rm $INC_DIR/merged.root
-hadd -j $INC_DIR/merged.root $INC_DIR/*.root
-root -l -q "plot_charm_eec_hists.C(\"$INC_DIR/merged.root\", \"$INC_DIR/\")"
+rm $DIR/merged.root
+hadd -j $DIR/merged.root $DIR/*.root
+root -l -q "plot_charm_eec_hists.C(\"$DIR/merged.root\", \"$DIR/\")"
 
-#root -l -q "plot_charm_eec_hists.C(\"./analysis/ep_10_100_pythia8_D0inpair/merged.root\", \"./analysis/ep_10_100_pythia8_D0inpair/\")"
+DIR=./analysis/ep_10_100_pythia8_pow1_D0injet
