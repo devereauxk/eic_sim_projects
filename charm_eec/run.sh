f977408e607d4cdedd76ed224aff095abe0448b5
Kyle Devereaux <devereauxk@berkeley.edu>
2023-04-07 13:34:14 -0700

    analyzing pythia8 18 on 275 output

--- charm_eec/run.sh
+++ charm_eec/run.sh
@@ -1,8 +1,12 @@
 
-DATA_DIR=../pythia8/ep_18_275/outfiles
-ANALYSIS_DIR=./analysis/ep_18_275
+DIR=./analysis/ep_18_275_pow1_inclusive
 
-mkdir $ANALYSIS_DIR
-rm $DATA_DIR/merged.root
-hadd -j $DATA_DIR/merged.root $DATA_DIR/*.root
-root -l -q "plot_charm_eec_hists.C(\"$DATA_DIR/merged.root\", \"$ANALYSIS_DIR/\")"
+rm $DIR/merged.root
+hadd -j $DIR/merged.root $DIR/*.root
+root -l -q "plot_charm_eec_hists.C(\"$DIR/merged.root\", \"$DIR/\")"
+
+DIR=./analysis/ep_18_275_pow1_D0injet
+
+rm $DIR/merged.root
+hadd -j $DIR/merged.root $DIR/*.root
+root -l -q "plot_charm_eec_hists.C(\"$DIR/merged.root\", \"$DIR/\")"
