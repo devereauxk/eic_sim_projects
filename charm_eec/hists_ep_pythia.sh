220ca889ddd17da5fbfde688501ac4ba6b612783
Kyle Devereaux <devereauxk@berkeley.edu>
2023-04-07 13:14:36 -0700

    analyzing pythia8 18 on 275 output

--- charm_eec/hists_ep_pythia.sh
+++ charm_eec/hists_ep_pythia.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 #IN_DIR=/gpfs/mnt/gpfs02/eic/wfan/data/pythia8HepMC_e10p100_ft_MB
-IN_DIR=/gpfs/mnt/gpfs02/eic/wfan/data/pythia8HepMC_e10p100_MB
-#OUT_DIR=/eic/u/kdevereaux/work/charm_eec/analysis/ep_10_100_pythia8_inclusive
+#IN_DIR=/gpfs/mnt/gpfs02/eic/wfan/data/pythia8HepMC_e10p100_MB
+#IN_DIR=/eic/u/kdevereaux/work/pythia8/ep_18_275/outfiles
+
+POW=$2
+DO_BOOST=$3
+DO_FORCE_INJET=$4
+DO_FORCE_INPAIR=$5
+FIXED_ID=$6
+IN_DIR=$7
+OUT_DIR=$8
 
 if [ -z "$1" ]
 then
@@ -21,21 +29,14 @@ echo $INPUT
 DIR=`printf "%04d" $INPUT`
 mkdir $DIR
 
-ln -s $IN_DIR/ep_minbias_highQ2_${INPUT}.root
+ln -s $IN_DIR/hepmcout_${INPUT}.root
 ln -s /eic/u/kdevereaux/work/charm_eec/charm_eec_hists.C
-
-POW=$2
-DO_BOOST=$3
-DO_FORCE_INJET=$4
-DO_FORCE_INPAIR=$5
-FIXED_ID=$6
-OUT_DIR=$7
 
 mkdir $OUT_DIR
 
 #                                                                                                        DOUBLE CHECK THESE
 #                                                                                                                            pow,boost,Q2x,jet,pair
-root -l -b -q "charm_eec_hists.C(\"ep_minbias_highQ2_${INPUT}.root\",\"$OUT_DIR/hists_eec_${INPUT}.root\", -1, 2131, 100, 0, $POW, $DO_BOOST, 1, $DO_FORCE_INJET, $DO_FORCE_INPAIR, $FIXED_ID)"
+root -l -b -q "charm_eec_hists.C(\"hepmcout_${INPUT}.root\",\"$OUT_DIR/hists_eec_${INPUT}.root\", -1, 2131, 100, 0, $POW, $DO_BOOST, 1, $DO_FORCE_INJET, $DO_FORCE_INPAIR, $FIXED_ID)"
 #root -l -b -q "eec_hists.C(\"hepmcout-${INPUT}.root\",\"$OUT_DIR/hists_eec_${INPUT}.root\", -1, 2131, 100, 0, 0.5, 1, 1)"
 
 #root -l -b -q "charm_eec_hists.C(\"/gpfs/mnt/gpfs02/eic/wfan/data/pythia8HepMC_e10p100_MB/ep_minbias_highQ2_99.root\",\"temp.root\", -1, 2131, 100, 0, 0.5, 0, 1, 1, 1, 421)"
