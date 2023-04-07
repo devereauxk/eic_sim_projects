8f95c8087c0294c93ae8886310a27a23004b96cf
Kyle Devereaux <devereauxk@berkeley.edu>
2023-04-07 16:43:09 -0700

    charm eec with pythia8 calc

--- charm_eec/run.sh
+++ charm_eec/run.sh
@@ -2,11 +2,11 @@
 DIR=./analysis/ep_18_275_pow1_inclusive
 
 rm $DIR/merged.root
-hadd -j $DIR/merged.root $DIR/*.root
+hadd $DIR/merged.root $DIR/*.root
 root -l -q "plot_charm_eec_hists.C(\"$DIR/merged.root\", \"$DIR/\")"
 
 DIR=./analysis/ep_18_275_pow1_D0injet
 
 rm $DIR/merged.root
-hadd -j $DIR/merged.root $DIR/*.root
-root -l -q "plot_charm_eec_hists.C(\"$DIR/merged.root\", \"$DIR/\")"
+hadd $DIR/merged.root $DIR/*.root
+root -l -q "plot_charm_eec_hists.C(\"$DIR/merged.root\", \"$DIR/\")"z
