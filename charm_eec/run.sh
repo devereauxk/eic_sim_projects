68a8920f995c0b329306ead7b2dcab11c6450fc3
Kyle Devereaux <devereauxk@berkeley.edu>
2023-04-08 17:46:31 -0700

    charm eec

--- charm_eec/run.sh
+++ charm_eec/run.sh
@@ -1,12 +1,14 @@
 
-DIR=./analysis/ep_18_275_pow1_inclusive
+DIR=./analysis/ep_18_275_nodecay_pow1_D0inpair
 
 rm $DIR/merged.root
 hadd $DIR/merged.root $DIR/*.root
 root -l -q "plot_charm_eec_hists.C(\"$DIR/merged.root\", \"$DIR/\")"
 
-DIR=./analysis/ep_18_275_pow1_D0injet
+#DIR=./analysis/ep_18_275_nodecay_pow1_D0injet
 
-rm $DIR/merged.root
-hadd $DIR/merged.root $DIR/*.root
-root -l -q "plot_charm_eec_hists.C(\"$DIR/merged.root\", \"$DIR/\")"
+#rm $DIR/merged.root
+#hadd $DIR/merged.root $DIR/*.root
+#root -l -q "plot_charm_eec_hists.C(\"$DIR/merged.root\", \"$DIR/\")"
+
+#root -l -q "plot_charm_eec_hists.C(\"./analysis/ep_18_275_pow1_inclusive/merged.root\", \"./analysis/ep_18_275_pow1_inclusive/\")"
