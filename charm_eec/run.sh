23d417d3d78a878c508409d5fc3cc0c219dda5b2
Kyle Devereaux <devereauxk@berkeley.edu>
2023-04-11 21:51:17 -0700

    Lc charm tagged jet eec

--- charm_eec/run.sh
+++ charm_eec/run.sh
@@ -1,18 +1,18 @@
 
 DIR=./analysis/ep_18_275_nodecay_pow1_inclusive
 
-#rm $DIR/merged.root
-#hadd $DIR/merged.root $DIR/*.root
+rm $DIR/merged.root
+hadd $DIR/merged.root $DIR/*.root
 root -l -q "plot_charm_eec_hists.C(\"$DIR/merged.root\", \"$DIR/\")"
 
 DIR=./analysis/ep_18_275_nodecay_pow1_D0injet
 
-#rm $DIR/merged.root
-#hadd $DIR/merged.root $DIR/*.root
+rm $DIR/merged.root
+hadd $DIR/merged.root $DIR/*.root
 root -l -q "plot_charm_eec_hists.C(\"$DIR/merged.root\", \"$DIR/\")"
 
 DIR=./analysis/ep_18_275_nodecay_pow1_Lcinjet
 
-#rm $DIR/merged.root
-#hadd $DIR/merged.root $DIR/*.root
+rm $DIR/merged.root
+hadd $DIR/merged.root $DIR/*.root
 root -l -q "plot_charm_eec_hists.C(\"$DIR/merged.root\", \"$DIR/\")"
