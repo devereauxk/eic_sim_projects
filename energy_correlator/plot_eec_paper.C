f2c9c8149355d32d338897664c3acb247b77fe10
Kyle Devereaux <devereauxk@berkeley.edu>
2023-06-21 13:52:59 +0200

    local push

--- energy_correlator/plot_eec_paper.C
+++ energy_correlator/plot_eec_paper.C
@@ -424,10 +424,10 @@ void pt_bin_side_by_side()
       temp = (TH1D*) h1d_jet_eec_rlsqrtpt[species_pick][k_pick][etabin_pick][ipt]->Clone();
       temp_baseline = (TH1D*) h1d_jet_eec_rlsqrtpt[0][0][etabin_pick][ipt]->Clone();
       temp->Print();
-      cout<<" bin 27 content: "<<temp->GetBinContent(27)<<cout;
+      cout<<" bin 27 content: "<<temp->GetBinContent(27)<<endl;
       fig3.push_back(temp);
       temp_baseline->Print();
-      cout<<" baseline bin 27 content: "<<temp_baseline->GetBinContent(27)<<cout;
+      cout<<" baseline bin 27 content: "<<temp_baseline->GetBinContent(27)<<endl;
       fig3_baseline.push_back(temp_baseline);
 
       // calculate relative normalization ratio
