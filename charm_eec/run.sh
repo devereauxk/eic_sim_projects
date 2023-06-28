01facb5a2634e33e17e60db6818ff19e4bcc6fb4
Kyle Devereaux <devereauxk@berkeley.edu>
2023-06-28 14:50:13 +0200

    local push

--- charm_eec/run.sh
+++ charm_eec/run.sh
@@ -1,11 +1,11 @@
 
-DIR=./analysis/ep_18_275_Kpi_pow1_inclusive
+DIR=./analysis/ep_18_275_Dstar_pow1_inclusive
 
 #rm $DIR/merged.root
 #hadd $DIR/merged.root $DIR/*.root
 root -l -q "plot_charm_eec_hists.C(\"$DIR/merged.root\", \"$DIR/\")"
 
-DIR=./analysis/ep_18_275_Kpi_pow1_D0injet
+DIR=./analysis/ep_18_275_Dstar_pow1_D0injet
 
 #rm $DIR/merged.root
 #hadd $DIR/merged.root $DIR/*.root
