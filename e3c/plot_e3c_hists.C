54c2171a27d174ea1d2e01858c53c0c0700c640c
Kyle Devereaux <devereauxk@berkeley.edu>
2023-06-29 14:13:59 +0200

    local push

--- e3c/plot_e3c_hists.C
+++ e3c/plot_e3c_hists.C
@@ -258,8 +258,8 @@ void raw_nice_3d_plots(const char* out_dir)
       float plot_xrange_hi = 1;
       float plot_yrange_lo = 0;
       float plot_yrange_hi = 1.5; // TMath::Pi() / 2.0;
-      //float plot_zrange_lo = 0;
-      //float plot_zrange_hi = 0.006;
+      float plot_zrange_lo = 0;
+      float plot_zrange_hi = 0.006;
 
       float bin_center = picked->GetXaxis()->GetBinCenter(ibin);
       sliced = (TH3D*) picked->Clone("temp3d");
@@ -270,7 +270,7 @@ void raw_nice_3d_plots(const char* out_dir)
 
       temp->GetXaxis()->SetRangeUser(plot_xrange_lo,plot_xrange_hi);
       temp->GetYaxis()->SetRangeUser(plot_yrange_lo,plot_yrange_hi);
-      temp->GetZaxis()->SetRangeUser(plot_zrange_lo, plot_zrange_hi);
+      //temp->GetZaxis()->SetRangeUser(plot_zrange_lo, plot_zrange_hi);
       temp->GetXaxis()->SetTitle("#xi");
       temp->GetYaxis()->SetTitle("#phi");
       temp->Draw("colz");
