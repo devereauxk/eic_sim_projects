5d76c44e33715c6a93e9e82b4fa1c5472adb505a
Kyle Devereaux <devereauxk@berkeley.edu>
2023-08-08 13:40:54 -0700

    local push

--- e3c/run.sh
+++ e3c/run.sh
@@ -9,6 +9,11 @@
 
 #root -l -q "plot_e3c_hists.C(\"./analysis/eC_1E8_K4/merged.root\", \"./analysis/eC_1E8_K4/\")"
 
+root -l -q "plot_e3c_hists.C(\"./analysis/ep_10_100_K0_pow025/merged.root\", \"./analysis/ep_10_100_K0_pow025/\")"
+root -l -q "plot_e3c_hists.C(\"./analysis/ep_10_100_K0_pow05/merged.root\", \"./analysis/ep_10_100_K0_pow05/\")"
+root -l -q "plot_e3c_hists.C(\"./analysis/ep_10_100_K0_pow1/merged.root\", \"./analysis/ep_10_100_K0_pow1/\")"
+root -l -q "plot_e3c_hists.C(\"./analysis/ep_10_100_K0_pow15/merged.root\", \"./analysis/ep_10_100_K0_pow15/\")"
+
 root -l -q "plot_e3c_hists.C(\"./analysis/eAu_10_100_K4_density_pow025/merged.root\", \"./analysis/eAu_10_100_K4_density_pow025/\")"
 root -l -q "plot_e3c_hists.C(\"./analysis/eAu_10_100_K4_density_pow05/merged.root\", \"./analysis/eAu_10_100_K4_density_pow05/\")"
 root -l -q "plot_e3c_hists.C(\"./analysis/eAu_10_100_K4_density_pow1/merged.root\", \"./analysis/eAu_10_100_K4_density_pow1/\")"
