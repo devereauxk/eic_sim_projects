bde5dcb7584e34f533ef8bdb13fb2a6661963257
Kyle Devereaux <devereauxk@berkeley.edu>
2023-08-09 21:40:41 -0400

    sdcc push

--- eHIJING/eHIJING-pythia/eHIJING-examples/run_eC_condor.sh
+++ eHIJING/eHIJING-pythia/eHIJING-examples/run_eC_condor.sh
@@ -29,7 +29,7 @@ Neve=400000
 K=4 # default: 4.0 for EIC
 M=1 # Generlizaed HT:1,  HIgher-twist:0, both in the soft gluon emission limit.
 
-label="eC_1E8_K4"
+label="eC_10_100_K4_density"
 folder=$OUT_DIR/Events/$label
 TablePath=$OUT_DIR/Tables/$label
 mkdir -p $folder
