174d6dc6db743b32f5a751f0bb3c271769094c87
Kyle Devereaux <devereauxk@berkeley.edu>
2023-08-31 15:00:58 -0700

    local push

---
+++ clas_eec/run.sh
@@ -0,0 +1,1 @@
+root -l -q "plot_eec_hists.C(\"./eHIJING/ep_11_0_K0_pow1/merged.root\", \"./eHIJING/ep_11_0_K0_pow1/\")"
---
+++ clas_eec/run_eec_hists_ep.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+IN_DIR=/eic/u/kdevereaux/work/eHIJING/eHIJING-pythia/eHIJING-examples/Events/ep_11_0_K0
+OUT_DIR=/eic/u/kdevereaux/work/energy_correlator/eHIJING/ep_11_0_K0_pow1
+
+if [ -z "$1" ]
+then
+        echo "No job number set."
+        echo "Please run as ./run_ep.sh jobnumber"
+        echo "Exiting..."
+        exit 1
+fi
+
+#Go into scratch directory
+chmod g+rx ${_CONDOR_SCRATCH_DIR}
+cd ${_CONDOR_SCRATCH_DIR}
+
+#Make subdirectory and move there
+INPUT=$(( 0 + $1 ))
+
+echo $INPUT
+DIR=`printf "%04d" $INPUT`
+mkdir $DIR
+
+ln -s $IN_DIR/ep_${INPUT}.dat
+ln -s /eic/u/kdevereaux/work/energy_correlator/eec_hists.C
+
+mkdir $OUT_DIR
+
+#                                                                                                        DOUBLE CHECK THESE
+#                                                                                                        power, boost, Q2x
+root -l -b -q "event_eec_hists.C(\"ep_${INPUT}.dat\",\"$OUT_DIR/hists_eec_${INPUT}.root\", 1, 11, 0, 0, 1, 0, 1)"
+
+#root -l -b -q "event_eec_hists.C(\"/eic/u/kdevereaux/work/eHIJING/eHIJING-pythia/eHIJING-examples/Events/ep_11_0_K0_pow1/ep_1.dat\",\"temp.root\", 1, 11, 0, 0, 1, 0, 1)"
