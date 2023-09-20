52827aa1ca8132b7ee2170d43e6024530e9b9208
Kyle Devereaux <devereauxk@berkeley.edu>
2023-09-20 12:51:24 -0700

    local push

--- e3c/plot_e3c_hists.C
+++ e3c/plot_e3c_hists.C
@@ -26,6 +26,7 @@ TH2D* h2d_Q2_x[etabin][ptbin] = {};
 TH1D* h1d_part_pt[etabin] = {};
 TH1D* h1d_part_eta[ptbin] = {};
 TH1D* h1d_part_mult = NULL;
+TH1D* h1d_part_energy[etabin][ptbin] = {};
 
 static int cno = 0;
 
@@ -537,6 +538,18 @@ void particle_hists(const char* out_dir)
     hists_to_csv(Form("%seta.csv", out_dir), hists);
   }
 
+  hists.clear();
+  // energy spectrum in eta and pt bins
+  for (int ieta = 0; ieta < 3; ieta++)
+  {
+    for (int ipt = 0; ipt < 3; ipt++)
+    {
+      temp = (TH1D*) h1d_part_energy[ieta][ipt]->Clone("temp");
+      hists.push_back(temp);
+    }
+  }
+  hists_to_csv(Form("%senergy.csv", out_dir), hists);
+
   // 1d event multiplicity histogram
   mclogy(cno++);
   {
@@ -600,6 +613,9 @@ void plot_e3c_hists(const char* fin_name = "hists_eec.root", const char* out_dir
 
       h2d_Q2_x[ieta][ipt] = (TH2D*) fin->Get(Form("h2d_Q2_x_%d_%d", ieta, ipt));
       h2d_Q2_x[ieta][ipt]->SetName(Form("h2d_Q2_x_%d_%d", ieta, ipt));
+
+      h1d_part_energy[ieta][ipt] = (TH1D*) fin->Get(Form("h2d_part_energy_%d_%d", ieta, ipt));
+      h1d_part_energy[ieta][ipt]->SetName(Form("h2d_part_energy_%d_%d", ieta, ipt));
     }
   }
 
